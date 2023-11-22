6fc80691533953aaed11e69b32cd6cb9a1cb3932
LegacyGamerHD <wesleyvanneck1012@gmail.com>
2023-11-22 12:52:43 +0100

    Update egl_bridge.c

--- app_pojavlauncher/src/main/jni/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/egl_bridge.c
@@ -209,8 +209,8 @@ int pojavInitOpenGL() {
         pojav_environ->config_renderer = RENDERER_VK_ZINK;
         setenv("GALLIUM_DRIVER", "panfrost", 1);
         setenv("PAN_DEBUG","gofaster",0);
-        setenv("PAN_DEBUG","gl3",
-            3.3)
+        setenv("MESA_GL_VERSION_OVERRIDE","3.3");
+        setenv("MESA_GLSL_VERSION_OVERRIDE","330");
         set_osm_bridge_tbl();
     }
     if(br_init()) {
