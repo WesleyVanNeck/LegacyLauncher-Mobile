f561fd6edf847819a74b48650e015a67f8c49a06
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:09 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/QuiltInstallFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/QuiltInstallFragment.java
@@ -1,24 +1,24 @@
 package net.kdt.pojavlaunch.fragments;
 
-import net.kdt.pojavlaunch.modloaders.FabriclikeUtils;
+import net.kdt.pojavlaunch.modloaders.FabriclikeModloader;
 import net.kdt.pojavlaunch.modloaders.ModloaderListenerProxy;
 
 public class QuiltInstallFragment extends FabriclikeInstallFragment {
 
     public static final String TAG = "QuiltInstallFragment";
-    private static ModloaderListenerProxy sTaskProxy;
+    private static ModloaderListenerProxy modloaderListenerProxy;
 
     public QuiltInstallFragment() {
-        super(FabriclikeUtils.QUILT_UTILS);
+        super(FabriclikeModloader.QUILT_UTILS);
     }
 
     @Override
-    protected ModloaderListenerProxy getListenerProxy() {
-        return sTaskProxy;
+    protected ModloaderListenerProxy getModloaderListenerProxy() {
+        return modloaderListenerProxy;
     }
 
     @Override
-    protected void setListenerProxy(ModloaderListenerProxy listenerProxy) {
-        sTaskProxy = listenerProxy;
+    protected void setModloaderListenerProxy(ModloaderListenerProxy listenerProxy) {
+        modloaderListenerProxy = listenerProxy;
     }
 }
