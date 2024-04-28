bbf405c238b512e787e5c2aa9a51bddaf25082a5
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:11 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/ExitActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/ExitActivity.java
@@ -18,27 +18,21 @@ public class ExitActivity extends AppCompatActivity {
 
     @SuppressLint("StringFormatInvalid") //invalid on some translations but valid on most, cant fix that atm
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int code = -1;
-        Bundle extras = getIntent().getExtras();
-        if(extras != null) {
-            code = extras.getInt("code",-1);
-        }
+        int code = getIntent().getIntExtra("code", -1);
 
         new AlertDialog.Builder(this)
-                .setMessage(getString(R.string.mcn_exit_title,code))
+                .setMessage(getString(R.string.mcn_exit_title, code))
                 .setPositiveButton(R.string.main_share_logs, (dialog, which) -> shareLog(this))
-                .setOnDismissListener(dialog -> ExitActivity.this.finish())
+                .setOnDismissListener(dialog -> finish())
                 .show();
     }
 
     public static void showExitMessage(Context ctx, int code) {
-        Intent i = new Intent(ctx,ExitActivity.class);
-        i.putExtra("code",code);
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Intent i = new Intent(ctx, ExitActivity.class);
+        i.putExtra("code", code);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         ctx.startActivity(i);
     }
-
 }
