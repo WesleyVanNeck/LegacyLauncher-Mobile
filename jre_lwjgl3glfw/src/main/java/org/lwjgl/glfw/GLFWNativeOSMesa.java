c7503d630cfa309ef8be0649635e84de3e4362d0
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:33 +0700

    improve

--- jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/GLFWNativeOSMesa.java
+++ jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/GLFWNativeOSMesa.java
@@ -1,33 +1,11 @@
 package org.lwjgl.glfw;
 
 import org.lwjgl.PointerBuffer;
+import org.lwjgl.system.JNI;
+import org.lwjgl.system.MemoryUtil;
 import org.lwjgl.system.NativeType;
 
 import java.nio.IntBuffer;
 
-import javax.annotation.Nullable;
-
-public class GLFWNativeOSMesa {
-    @NativeType("int")
-    public static boolean glfwGetOSMesaColorBuffer(@NativeType("GLFWwindow *") long window, @Nullable @NativeType("int *") IntBuffer width, @Nullable @NativeType("int *") IntBuffer height, @Nullable @NativeType("int *") IntBuffer format, @Nullable @NativeType("void **") PointerBuffer buffer) {
-        throw new UnsupportedOperationException("Not implemented yet!");
-    }
-
-    public static int glfwGetOSMesaDepthBuffer(@NativeType("GLFWwindow *") long window, @Nullable @NativeType("int *") IntBuffer width, @Nullable @NativeType("int *") IntBuffer height, @Nullable @NativeType("int *") IntBuffer bytesPerValue, @Nullable @NativeType("void **") PointerBuffer buffer) {
-        throw new UnsupportedOperationException("Not implemented yet!");
-    }
-
-    @NativeType("OSMesaContext")
-    public static long glfwGetOSMesaContext(@NativeType("GLFWwindow *") long window) {
-        throw new UnsupportedOperationException("Not implemented yet!");
-    }
-
-    @NativeType("int")
-    public static boolean glfwGetOSMesaColorBuffer(@NativeType("GLFWwindow *") long window, @Nullable @NativeType("int *") int[] width, @Nullable @NativeType("int *") int[] height, @Nullable @NativeType("int *") int[] format, @Nullable @NativeType("void **") PointerBuffer buffer) {
-        throw new UnsupportedOperationException("Not implemented yet!");
-    }
-
-    public static int glfwGetOSMesaDepthBuffer(@NativeType("GLFWwindow *") long window, @Nullable @NativeType("int *") int[] width, @Nullable @NativeType("int *") int[] height, @Nullable @NativeType("int *") int[] bytesPerValue, @Nullable @NativeType("void **") PointerBuffer buffer) {
-        throw new UnsupportedOperationException("Not implemented yet!");
-    }
-}
+import static org.lwjgl.glfw.GLFW.APIENTRY;
+import static org.lwjgl.system.MemoryUtil.NULL;
