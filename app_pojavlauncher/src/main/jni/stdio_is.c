37f5a308f035154455720cb43eed477176e3ef6c
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:45 +0700

    improve

--- app_pojavlauncher/src/main/jni/stdio_is.c
+++ app_pojavlauncher/src/main/jni/stdio_is.c
@@ -27,39 +27,52 @@ static volatile jobject logListener = NULL;
 static int latestlog_fd = -1;
 static _Atomic bool exit_tripped = false;
 
-
+// Returns true if the buffer should be recorded, false otherwise
 static bool recordBuffer(char* buf, ssize_t len) {
-    if(strstr(buf, "Session ID is")) return false;
-    if(latestlog_fd != -1) {
+    if (strstr(buf, "Session ID is")) {
+        return false;
+    }
+    if (latestlog_fd != -1) {
         write(latestlog_fd, buf, len);
         fdatasync(latestlog_fd);
     }
     return true;
 }
 
+// Called when the JVM is loaded
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, __attribute((unused)) void* reserved) {
     stdiois_jvm = vm;
     JNIEnv *env;
-    (*vm)->GetEnv(vm, (void**)&env, JNI_VERSION_1_4);
+    jint result = (*vm)->GetEnv(vm, (void**)&env, JNI_VERSION_1_4);
+    if (result != JNI_OK) {
+        return result;
+    }
     jclass eventLogListener = (*env)->FindClass(env, "net/kdt/pojavlaunch/Logger$eventLogListener");
+    if (eventLogListener == NULL) {
+        return JNI_EDETACHED;
+    }
     logger_onEventLogged = (*env)->GetMethodID(env, eventLogListener, "onEventLogged", "(Ljava/lang/String;)V");
+    if (logger_onEventLogged == NULL) {
+        return JNI_EDETACHED;
+    }
     return JNI_VERSION_1_4;
 }
 
-static void *logger_thread() {
+// The logger thread function
+static void *logger_thread(void *arg) {
     JNIEnv *env;
     jstring writeString;
     (*stdiois_jvm)->AttachCurrentThread(stdiois_jvm, &env, NULL);
     ssize_t  rsize;
     char buf[2050];
     while((rsize = read(pfd[0], buf, sizeof(buf)-1)) > 0) {
-        bool shouldRecordString = recordBuffer(buf, rsize); //record with newline int latestlog
+        bool shouldRecordString = recordBuffer(buf, rsize);
         if(buf[rsize-1]=='\n') {
-            rsize=rsize-1; //truncate
+            rsize=rsize-1;
         }
         buf[rsize]=0x00;
         if(shouldRecordString && logListener != NULL) {
-            writeString = (*env)->NewStringUTF(env, buf); //send to app without newline
+            writeString = (*env)->NewStringUTF(env, buf);
             (*env)->CallVoidMethod(env, logListener, logger_onEventLogged, writeString);
             (*env)->DeleteLocalRef(env, writeString);
         }
@@ -67,25 +80,22 @@ static void *logger_thread() {
     (*stdiois_jvm)->DetachCurrentThread(stdiois_jvm);
     return NULL;
 }
+
+// Starts the logger
 JNIEXPORT void JNICALL
 Java_net_kdt_pojavlaunch_Logger_begin(JNIEnv *env, __attribute((unused)) jclass clazz, jstring logPath) {
     if(latestlog_fd != -1) {
-        int localfd = latestlog_fd;
-        latestlog_fd = -1;
-        close(localfd);
+        close(latestlog_fd);
     }
     jclass ioeClass = (*env)->FindClass(env, "java/io/IOException");
 
+    setvbuf(stdout, 0, _IOLBF, 0);
+    setvbuf(stderr, 0, _IONBF, 0);
 
-    setvbuf(stdout, 0, _IOLBF, 0); // make stdout line-buffered
-    setvbuf(stderr, 0, _IONBF, 0); // make stderr unbuffered
-
-    /* create the pipe and redirect stdout and stderr */
     pipe(pfd);
     dup2(pfd[1], 1);
     dup2(pfd[1], 2);
 
-    /* open latestlog.txt for writing */
     const char* logFilePath = (*env)->GetStringUTFChars(env, logPath, NULL);
     latestlog_fd = open(logFilePath, O_WRONLY | O_TRUNC);
     if(latestlog_fd == -1) {
@@ -95,8 +105,7 @@ Java_net_kdt_pojavlaunch_Logger_begin(JNIEnv *env, __attribute((unused)) jclass 
     }
     (*env)->ReleaseStringUTFChars(env, logPath, logFilePath);
 
-    /* spawn the logging thread */
-    int result = pthread_create(&logger, 0, logger_thread, 0);
+    int result = pthread_create(&logger, 0, logger_thread, NULL);
     if(result != 0) {
         close(latestlog_fd);
         (*env)->ThrowNew(env, ioeClass, strerror(result));
@@ -104,63 +113,42 @@ Java_net_kdt_pojavlaunch_Logger_begin(JNIEnv *env, __attribute((unused)) jclass 
     pthread_detach(logger);
 }
 
-
-
-
 typedef void (*exit_func)(int);
 
-_Noreturn static void nominal_exit(int code) {
+// Called when the program exits
+static void nominal_exit(int code) {
     JNIEnv *env;
     jint errorCode = (*exitTrap_jvm)->GetEnv(exitTrap_jvm, (void**)&env, JNI_VERSION_1_6);
     if(errorCode == JNI_EDETACHED) {
         errorCode = (*exitTrap_jvm)->AttachCurrentThread(exitTrap_jvm, &env, NULL);
     }
     if(errorCode != JNI_OK) {
-        // Step on a landmine and die, since we can't invoke the Dalvik exit without attaching to
-        // Dalvik.
-        // I mean, if Zygote can do that, why can't I?
         killpg(getpgrp(), SIGTERM);
     }
     if(code != 0) {
-        // Exit code 0 is pretty established as "eh it's fine"
-        // so only open the GUI if the code is != 0
         (*env)->CallStaticVoidMethod(env, exitTrap_exitClass, exitTrap_staticMethod, exitTrap_ctx, code);
     }
-    // Delete the reference, not gonna need 'em later anyway
     (*env)->DeleteGlobalRef(env, exitTrap_ctx);
     (*env)->DeleteGlobalRef(env, exitTrap_exitClass);
-
-    // A hat trick, if you will
-    // Call the Android System.exit() to perform Android's shutdown hooks and do a
-    // fully clean exit.
-    // After doing this, either of these will happen:
-    // 1. Runtime calls exit() for real and it will be handled by ByteHook's recurse handler
-    // and redirected back to the OS
-    // 2. Zygote sends SIGTERM (no handling necessary, the process perishes)
-    // 3. A different thread calls exit() and the hook will go through the exit_tripped path
     jclass systemClass = (*env)->FindClass(env,"java/lang/System");
     jmethodID exitMethod = (*env)->GetStaticMethodID(env, systemClass, "exit", "(I)V");
     (*env)->CallStaticVoidMethod(env, systemClass, exitMethod, 0);
-    // System.exit() should not ever return, but the compiler doesn't know about that
-    // so put a while loop here
     while(1) {}
 }
 
+// Called when the program exits
 static void custom_exit(int code) {
-    // If the exit was already done (meaning it is recursive or from a different thread), pass the call through
     if(exit_tripped) {
         BYTEHOOK_CALL_PREV(custom_exit, exit_func, code);
         BYTEHOOK_POP_STACK();
         return;
     }
     exit_tripped = true;
-    // Perform a nominal exit, as we expect.
     nominal_exit(code);
-    BYTEHOOK_POP_STACK();
 }
 
+// Called when the program exits
 static void custom_atexit() {
-    // Same as custom_exit, but without the code or the exit passthrough.
     if(exit_tripped) {
         return;
     }
@@ -168,6 +156,7 @@ static void custom_atexit() {
     nominal_exit(0);
 }
 
+// Sets up the exit trap
 JNIEXPORT void JNICALL Java_net_kdt_pojavlaunch_utils_JREUtils_setupExitTrap(JNIEnv *env, __attribute((unused)) jclass clazz, jobject context) {
     exitTrap_ctx = (*env)->NewGlobalRef(env,context);
     (*env)->GetJavaVM(env,&exitTrap_jvm);
@@ -181,13 +170,11 @@ JNIEXPORT void JNICALL Java_net_kdt_pojavlaunch_utils_JREUtils_setupExitTrap(JNI
                           NULL,
                           NULL);
     }else {
-        // If we can't hook, register atexit(). This won't report a proper error code,
-        // but it will prevent a SIGSEGV or a SIGABRT from the depths of Dalvik that happens
-        // on exit().
         atexit(custom_atexit);
     }
 }
 
+// Appends to the log
 JNIEXPORT void JNICALL Java_net_kdt_pojavlaunch_Logger_appendToLog(JNIEnv *env, __attribute((unused)) jclass clazz, jstring text) {
     jsize appendStringLength = (*env)->GetStringUTFLength(env, text);
     char newChars[appendStringLength+2];
@@ -199,6 +186,7 @@ JNIEXPORT void JNICALL Java_net_kdt_pojavlaunch_Logger_appendToLog(JNIEnv *env, 
     }
 }
 
+// Sets the log listener
 JNIEXPORT void JNICALL
 Java_net_kdt_pojavlaunch_Logger_setLogListener(JNIEnv *env, __attribute((unused)) jclass clazz, jobject log_listener) {
     jobject logListenerLocal = logListener;
