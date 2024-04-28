0e85c8950444c5078f9fbc62cdcaed1f7022e86a
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:17 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/value/launcherprofiles/MinecraftSelectedUser.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/value/launcherprofiles/MinecraftSelectedUser.java
@@ -4,6 +4,31 @@ import androidx.annotation.Keep;
 
 @Keep
 public class MinecraftSelectedUser {
-	public String account;
-	public String profile;
+
+    // Use private access modifier for encapsulation
+    private String account;
+    private String profile;
+
+    // Add a constructor to initialize the fields
+    public MinecraftSelectedUser(String account, String profile) {
+        this.account = account;
+        this.profile = profile;
+    }
+
+    // Generate getter and setter methods for the fields
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
 }
