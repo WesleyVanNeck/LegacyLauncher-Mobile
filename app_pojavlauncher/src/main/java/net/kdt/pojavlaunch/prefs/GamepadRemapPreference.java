334544cc298ab82fe159ad8a92db42cbe2eb0c71
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:23 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/GamepadRemapPreference.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/GamepadRemapPreference.java
@@ -9,26 +9,28 @@ import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 
 import net.kdt.pojavlaunch.R;
-
 import fr.spse.gamepad_remapper.Remapper;
 
 public class GamepadRemapPreference extends Preference {
+
+    public GamepadRemapPreference(@NonNull Context context) {
+        this(context, null);
+    }
 
     public GamepadRemapPreference(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init();
     }
 
-    public GamepadRemapPreference(@NonNull Context context) {
-        super(context);
-        init();
-    }
-
-    private void init(){
+    private void init() {
         setOnPreferenceClickListener(preference -> {
             Remapper.wipePreferences(getContext());
-            Toast.makeText(getContext(), R.string.preference_controller_map_wiped, Toast.LENGTH_SHORT).show();
+            showToast(R.string.preference_controller_map_wiped);
             return true;
         });
+    }
+
+    private void showToast(int messageResId) {
+        Toast.makeText(getContext(), messageResId, Toast.LENGTH_SHORT).show();
     }
 }
