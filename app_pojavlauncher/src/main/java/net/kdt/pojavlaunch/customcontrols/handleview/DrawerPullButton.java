82ec0f850fafbe0f94cdd471d31a810109dcef80
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:13 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/handleview/DrawerPullButton.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/handleview/DrawerPullButton.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.VectorDrawableCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -13,26 +14,41 @@ import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
 import net.kdt.pojavlaunch.R;
 
 public class DrawerPullButton extends View {
-    public DrawerPullButton(Context context) {super(context); init();}
-    public DrawerPullButton(Context context, @Nullable AttributeSet attrs) {super(context, attrs); init();}
-
-    private final Paint mPaint = new Paint();
+    private Paint mPaint;
     private VectorDrawableCompat mDrawable;
 
-    private void init(){
-        mDrawable = VectorDrawableCompat.create(getContext().getResources(), R.drawable.ic_sharp_settings_24, null);
+    public DrawerPullButton(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public DrawerPullButton(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mPaint = new Paint();
+        mDrawable = VectorDrawableCompat.create(context.getResources(), R.drawable.ic_sharp_settings_24, null);
         setAlpha(0.33f);
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mDrawable.setBounds(0, 0, h, h);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
         mPaint.setColor(Color.BLACK);
-        canvas.drawArc(0,-getHeight(),getWidth(), getHeight(), 0, 180, true, mPaint);
+        canvas.drawArc(0, 0, getWidth(), getHeight(), 0, 180, true, mPaint);
 
         mPaint.setColor(Color.WHITE);
-        mDrawable.setBounds(0, 0, getHeight(), getHeight());
         canvas.save();
-        canvas.translate((getWidth()-getHeight())/2f, 0);
+        canvas.translate((getWidth() - getHeight()) / 2f, 0);
         mDrawable.draw(canvas);
         canvas.restore();
     }
