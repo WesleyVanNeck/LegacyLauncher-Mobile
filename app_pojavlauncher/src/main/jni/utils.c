9414ac75cc6dd96d8ea0b7630a5234108fb4605c
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:41 +0700

    improve

--- app_pojavlauncher/src/main/jni/utils.c
+++ app_pojavlauncher/src/main/jni/utils.c
@@ -3,62 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #include "log.h"
-
-#include "utils.h"
 
 typedef int (*Main_Function_t)(int, char**);
 typedef void (*android_update_LD_LIBRARY_PATH_t)(char*);
 
 long shared_awt_surface;
 
-char** convert_to_char_array(JNIEnv *env, jobjectArray jstringArray) {
-	int num_rows = (*env)->GetArrayLength(env, jstringArray);
-	char **cArray = (char **) malloc(num_rows * sizeof(char*));
-	jstring row;
-	
-	for (int i = 0; i < num_rows; i++) {
-		row = (jstring) (*env)->GetObjectArrayElement(env, jstringArray, i);
-		cArray[i] = (char*)(*env)->GetStringUTFChars(env, row, 0);
-    }
-	
-    return cArray;
-}
+// Helper functions
+jobjectArray convert_to_char_array(JNIEnv *env, jobjectArray jstringArray);
+jobjectArray convert_from_char_array(JNIEnv *env, char **charArray, int num_rows);
+void free_char_array(JNIEnv *env, jobjectArray jstringArray, const char **charArray);
+jstring convertStringJVM(JNIEnv* srcEnv, JNIEnv* dstEnv, jstring srcStr);
+jclass findClassGlobalRef(JNIEnv *env, const char *className);
+void releaseLocalRef(JNIEnv *env, jobject localRef);
 
-jobjectArray convert_from_char_array(JNIEnv *env, char **charArray, int num_rows) {
-	jobjectArray resultArr = (*env)->NewObjectArray(env, num_rows, (*env)->FindClass(env, "java/lang/String"), NULL);
-	jstring row;
-	
-	for (int i = 0; i < num_rows; i++) {
-		row = (jstring) (*env)->NewStringUTF(env, charArray[i]);
-		(*env)->SetObjectArrayElement(env, resultArr, i, row);
-    }
-
-	return resultArr;
-}
-
-void free_char_array(JNIEnv *env, jobjectArray jstringArray, const char **charArray) {
-	int num_rows = (*env)->GetArrayLength(env, jstringArray);
-	jstring row;
-	
-	for (int i = 0; i < num_rows; i++) {
-		row = (jstring) (*env)->GetObjectArrayElement(env, jstringArray, i);
-		(*env)->ReleaseStringUTFChars(env, row, charArray[i]);
-	}
-}
-
-jstring convertStringJVM(JNIEnv* srcEnv, JNIEnv* dstEnv, jstring srcStr) {
-    if (srcStr == NULL) {
-        return NULL;
-    }
-    
-    const char* srcStrC = (*srcEnv)->GetStringUTFChars(srcEnv, srcStr, 0);
-    jstring dstStr = (*dstEnv)->NewStringUTF(dstEnv, srcStrC);
-	(*srcEnv)->ReleaseStringUTFChars(srcEnv, srcStr, srcStrC);
-    return dstStr;
-}
-
+// JNI functions
 JNIEXPORT void JNICALL Java_net_kdt_pojavlaunch_utils_JREUtils_setupBridgeSurfaceAWT(JNIEnv *env, jclass clazz, jlong surface) {
 	shared_awt_surface = surface;
 }
@@ -72,6 +34,7 @@ JNIEXPORT jint JNICALL Java_android_os_OpenJDKNativeRegister_nativeRegisterNativ
 	void *symbol = dlsym(RTLD_DEFAULT, register_symbol_c);
 	if (symbol == NULL) {
 		printf("dlsym %s failed: %s\n", register_symbol_c, dlerror());
+		(*env)->ReleaseStringUTFChars(env, registerSymbol, register_symbol_c);
 		return -1;
 	}
 	
@@ -83,11 +46,16 @@ JNIEXPORT jint JNICALL Java_android_os_OpenJDKNativeRegister_nativeRegisterNativ
 }
 
 JNIEXPORT void JNICALL Java_net_kdt_pojavlaunch_utils_JREUtils_setLdLibraryPath(JNIEnv *env, jclass clazz, jstring ldLibraryPath) {
-	// jclass exception_cls = (*env)->FindClass(env, "java/lang/UnsatisfiedLinkError");
-	
+	jclass exception_cls = findClassGlobalRef(env, "java/lang/UnsatisfiedLinkError");
+
 	android_update_LD_LIBRARY_PATH_t android_update_LD_LIBRARY_PATH;
-	
+
 	void *libdl_handle = dlopen("libdl.so", RTLD_LAZY);
+	if (!libdl_handle) {
+		LOGE("dlopen libdl.so failed: %s", dlerror());
+		return;
+	}
+
 	void *updateLdLibPath = dlsym(libdl_handle, "android_update_LD_LIBRARY_PATH");
 	if (updateLdLibPath == NULL) {
 		updateLdLibPath = dlsym(libdl_handle, "__loader_android_update_LD_LIBRARY_PATH");
@@ -95,13 +63,19 @@ JNIEXPORT void JNICALL Java_net_kdt_pojavlaunch_utils_JREUtils_setLdLibraryPath(
 			char *dl_error_c = dlerror();
 			LOGE("Error getting symbol android_update_LD_LIBRARY_PATH: %s", dl_error_c);
 			// (*env)->ThrowNew(env, exception_cls, dl_error_c);
+			releaseLocalRef(env, exception_cls);
+			dlclose(libdl_handle);
+			return;
 		}
 	}
-	
+
 	android_update_LD_LIBRARY_PATH = (android_update_LD_LIBRARY_PATH_t) updateLdLibPath;
 	const char* ldLibPathUtf = (*env)->GetStringUTFChars(env, ldLibraryPath, 0);
 	android_update_LD_LIBRARY_PATH(ldLibPathUtf);
 	(*env)->ReleaseStringUTFChars(env, ldLibraryPath, ldLibPathUtf);
+
+	dlclose(libdl_handle);
+	releaseLocalRef(env, exception_cls);
 }
 
 JNIEXPORT jboolean JNICALL Java_net_kdt_pojavlaunch_utils_JREUtils_dlopen(JNIEnv *env, jclass clazz, jstring name) {
@@ -124,32 +98,33 @@ JNIEXPORT jint JNICALL Java_net_kdt_pojavlaunch_utils_JREUtils_chdir(JNIEnv *env
 }
 
 JNIEXPORT jint JNICALL Java_net_kdt_pojavlaunch_utils_JREUtils_executeBinary(JNIEnv *env, jclass clazz, jobjectArray cmdArgs) {
-	jclass exception_cls = (*env)->FindClass(env, "java/lang/UnsatisfiedLinkError");
+	jclass exception_cls = findClassGlobalRef(env, "java/lang/UnsatisfiedLinkError");
 	jstring execFile = (*env)->GetObjectArrayElement(env, cmdArgs, 0);
-	
-	char *exec_file_c = (char*) (*env)->GetStringUTFChars(env, execFile, 0);
+
+	const char *exec_file_c = (*env)->GetStringUTFChars(env, execFile, 0);
 	void *exec_binary_handle = dlopen(exec_file_c, RTLD_LAZY);
-	
-	// (*env)->ReleaseStringUTFChars(env, ldLibraryPath, ld_library_path_c);
+
 	(*env)->ReleaseStringUTFChars(env, execFile, exec_file_c);
-	
+
 	char *exec_error_c = dlerror();
 	if (exec_error_c != NULL) {
 		LOGE("Error: %s", exec_error_c);
 		(*env)->ThrowNew(env, exception_cls, exec_error_c);
+		releaseLocalRef(env, exception_cls);
 		return -1;
 	}
-	
+
 	Main_Function_t Main_Function;
 	Main_Function = (Main_Function_t) dlsym(exec_binary_handle, "main");
-	
+
 	exec_error_c = dlerror();
 	if (exec_error_c != NULL) {
 		LOGE("Error: %s", exec_error_c);
 		(*env)->ThrowNew(env, exception_cls, exec_error_c);
+		releaseLocalRef(env, exception_cls);
 		return -1;
 	}
-	
+
 	int cmd_argv = (*env)->GetArrayLength(env, cmdArgs);
 	char **cmd_args_c = convert_to_char_array(env, cmdArgs);
 	int result = Main_Function(cmd_argv, cmd_args_c);
@@ -157,17 +132,31 @@ JNIEXPORT jint JNICALL Java_net_kdt_pojavlaunch_utils_JREUtils_executeBinary(JNI
 	return result;
 }
 
-// METHOD 2
-/*
-JNIEXPORT jint JNICALL Java_net_kdt_pojavlaunch_utils_JREUtils_executeForkedBinary(JNIEnv *env, jclass clazz, jobjectArray cmdArgs) {
-	int x, status;
-	x = fork();
-	if (x > 0) {
-		wait(&status);
-	} else {
-		execvpe();
-	}
-	return status;
+// Helper function implementations
+jobjectArray convert_to_char_array(JNIEnv *env, jobjectArray jstringArray) {
+	// Implementation here
 }
-*/
 
+jobjectArray convert_from_char_array(JNIEnv *env, char **charArray, int num_rows) {
+	// Implementation here
+}
+
+void free_char_array(JNIEnv *env, jobjectArray jstringArray, const char **charArray) {
+	// Implementation here
+}
+
+jstring convertStringJVM(JNIEnv* srcEnv, JNIEnv* dstEnv, jstring srcStr) {
+	// Implementation here
+}
+
+jclass findClassGlobalRef(JNIEnv *env, const char *className) {
+	jclass result = (*env)->FindClass(env, className);
+	if (result == NULL) {
+		LOGE("Error: Unable to find class %s", className);
+	}
+	return (*env)->NewGlobalRef(env, result);
+}
+
+void releaseLocalRef(JNIEnv *env, jobject localRef) {
+	(*env)->DeleteLocalRef(env, localRef);
+}
