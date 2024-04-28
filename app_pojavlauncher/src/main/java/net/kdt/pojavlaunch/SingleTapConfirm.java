c1316907b3a2f8450a782fba5a28c6066d617b37
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:07 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/SingleTapConfirm.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/SingleTapConfirm.java
@@ -1,11 +1,11 @@
 package net.kdt.pojavlaunch;
 
-import android.view.*;
-import android.view.GestureDetector.*;
+import android.view.GestureDetector;
 
-public class SingleTapConfirm extends SimpleOnGestureListener {
-	@Override
-	public boolean onSingleTapUp(MotionEvent event) {
-		return true;
-	}
+public record SingleTapConfirm() implements GestureDetector.SimpleOnGestureListener {
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        return true;
+    }
 }
