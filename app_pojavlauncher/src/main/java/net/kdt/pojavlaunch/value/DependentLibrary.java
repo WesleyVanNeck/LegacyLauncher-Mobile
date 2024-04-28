6c6ac84e668d3e30037f831158e8ff65fb035da0
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:16 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/value/DependentLibrary.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/value/DependentLibrary.java
@@ -5,17 +5,43 @@ import net.kdt.pojavlaunch.JMinecraftVersionList.Arguments.ArgValue.ArgRules;
 
 @Keep
 public class DependentLibrary {
+    /**
+     * ArgRules array
+     */
+    @Keep
     public ArgRules[] rules;
+
+    /**
+     * Name of the library
+     */
+    @Keep
     public String name;
+
+    /**
+     * Library downloads
+     */
+    @Keep
     public LibraryDownloads downloads;
+
+    /**
+     * URL of the library
+     */
+    @Keep
     public String url;
 
     @Keep
-	public static class LibraryDownloads {
-		public final MinecraftLibraryArtifact artifact;
-		public LibraryDownloads(MinecraftLibraryArtifact artifact) {
-			this.artifact = artifact;
-		}
-	}
-}
+    public static class LibraryDownloads {
+        /**
+         * Minecraft library artifact
+         */
+        public final MinecraftLibraryArtifact artifact;
 
+        /**
+         * Constructor for LibraryDownloads
+         * @param artifact Minecraft library artifact
+         */
+        public LibraryDownloads(MinecraftLibraryArtifact artifact) {
+            this.artifact = artifact;
+        }
+    }
+}
