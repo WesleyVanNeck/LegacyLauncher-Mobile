488cbb43fc6378ee2c2069ea14285d847b779cec
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:12 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/imagecache/ImageReceiver.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/imagecache/ImageReceiver.java
@@ -3,8 +3,31 @@ package net.kdt.pojavlaunch.modloaders.modpacks.imagecache;
 import android.graphics.Bitmap;
 
 /**
- * ModIconCache will call your view back when the image becomes available with this interface
+ * A class for caching images used by mod icons.
  */
-public interface ImageReceiver {
-    void onImageAvailable(Bitmap image);
+public class ModIconCache {
+
+    /**
+     * An interface for receiving images as they become available.
+     *
+     * @param <T> the type of the image receiver
+     */
+    public interface ImageReceiver<T> {
+        /**
+         * Called when the image becomes available.
+         *
+         * @param image the image that has become available
+         */
+        void onImageAvailable(Bitmap image);
+    }
 }
+
+
+ModIconCache.ImageReceiver<MyImageReceiver> receiver = new ModIconCache.ImageReceiver<MyImageReceiver>() {
+    @Override
+    public void onImageAvailable(Bitmap image) {
+        if (image != null) {
+            // Do something with the image
+        }
+    }
+};
