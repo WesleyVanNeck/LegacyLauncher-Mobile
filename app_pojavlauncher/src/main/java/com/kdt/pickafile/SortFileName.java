9b911c6fe8d06c1ad4d6de2f1cb6ad7e318a9862
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:07 +0700

    improve

--- app_pojavlauncher/src/main/java/com/kdt/pickafile/SortFileName.java
+++ app_pojavlauncher/src/main/java/com/kdt/pickafile/SortFileName.java
@@ -1,13 +1,25 @@
 package com.kdt.pickafile;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
-public class SortFileName implements Comparator<File>
-{
-    @Override
-    public int compare(File f1, File f2) {
-		return f1.getName().compareToIgnoreCase(f2.getName());
+public class FileNameSorter {
+
+    public static void main(String[] args) {
+        File directory = new File("path/to/directory");
+        List<File> files = Arrays.asList(directory.listFiles());
+        files.sort(new SortFileName());
+        for (File file : files) {
+            System.out.println(file.getName());
+        }
     }
 }
 
+class SortFileName implements Comparator<File> {
+    @Override
+    public int compare(File f1, File f2) {
+        return f1.getName().compareToIgnoreCase(f2.getName());
+    }
+}
