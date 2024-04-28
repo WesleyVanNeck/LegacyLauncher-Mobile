aec2b3a9b132228e7f2c5189dd9a95d8f17cc469
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:11 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/models/SearchFilters.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/models/SearchFilters.java
@@ -3,11 +3,38 @@ package net.kdt.pojavlaunch.modloaders.modpacks.models;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Search filters, passed to APIs
+ * Search filters class, containing properties for filters to be passed to APIs.
  */
 public class SearchFilters {
-    public boolean isModpack;
-    public String name;
-    @Nullable public String mcVersion;
 
-}
+    // isModpack property
+    private boolean isModpack;
+
+    // name property
+    private String name;
+
+    // mcVersion property
+    @Nullable
+    private String mcVersion;
+
+    /**
+     * Constructs a SearchFilters object with the given parameters.
+     *
+     * @param isModpack whether the search is for a modpack
+     * @param name the name of the mod or modpack
+     * @param mcVersion the Minecraft version of the mod or modpack
+     */
+    public SearchFilters(boolean isModpack, String name, @Nullable String mcVersion) {
+        this.isModpack = isModpack;
+        this.name = name;
+        this.mcVersion = mcVersion;
+    }
+
+    /**
+     * Gets the value of the isModpack property.
+     *
+     * @return whether the search is for a modpack
+     */
+    public boolean isModpack() {
+        return isModpack;
+
