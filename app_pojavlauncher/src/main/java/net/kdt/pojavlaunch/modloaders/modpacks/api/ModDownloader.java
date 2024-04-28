3c4bd7788df0ecda01dac38a185dae2c78148544
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:34 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/api/ModDownloader.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/api/ModDownloader.java
@@ -2,179 +2,204 @@ package net.kdt.pojavlaunch.modloaders.modpacks.api;
 
 import androidx.annotation.Nullable;
 
-import net.kdt.pojavlaunch.Tools;
-import net.kdt.pojavlaunch.utils.DownloadUtils;
-
 import java.io.File;
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.util.concurrent.Callable;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * A class for downloading mods asynchronously.
+ */
 public class ModDownloader {
-    private static final ThreadLocal<byte[]> sThreadLocalBuffer = new ThreadLocal<>();
-    private final ThreadPoolExecutor mDownloadPool = new ThreadPoolExecutor(4,4,100, TimeUnit.MILLISECONDS,
-            new LinkedBlockingQueue<>());
-    private final AtomicBoolean mTerminator = new AtomicBoolean(false);
-    private final AtomicLong mDownloadSize = new AtomicLong(0);
-    private final Object mExceptionSyncPoint = new Object();
+    private static final int DOWNLOAD_RETRY_COUNT = 5;
+    private static final int DOWNLOAD_RETRY_DELAY_MILLIS = 1000;
+
+    private final ExecutorService mDownloadPool;
+    private final AtomicBoolean mTerminator;
+    private final AtomicLong mDownloadSize;
     private final File mDestinationDirectory;
     private final boolean mUseFileCount;
-    private IOException mFirstIOException;
-    private long mTotalSize;
+    private volatile IOException mFirstIOException;
+    private volatile long mTotalSize;
 
+    /**
+     * Constructs a new ModDownloader instance with the given destination directory.
+     *
+     * @param destinationDirectory the directory where the downloaded files will be saved
+     */
     public ModDownloader(File destinationDirectory) {
         this(destinationDirectory, false);
     }
 
+    /**
+     * Constructs a new ModDownloader instance with the given destination directory and file count mode.
+     *
+     * @param destinationDirectory the directory where the downloaded files will be saved
+     * @param useFileCount         whether to use file count mode or not
+     */
     public ModDownloader(File destinationDirectory, boolean useFileCount) {
-        this.mDownloadPool.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+        this.mDownloadPool = Executors.newFixedThreadPool(4);
+        this.mTerminator = new AtomicBoolean(false);
+        this.mDownloadSize = new AtomicLong(0);
         this.mDestinationDirectory = destinationDirectory;
         this.mUseFileCount = useFileCount;
+        if (mUseFileCount) {
+            mTotalSize = 0;
+        } else {
+            mTotalSize = -1;
+        }
     }
 
-    public void submitDownload(int fileSize, String relativePath, @Nullable String downloadHash, String... url) {
-        if(mUseFileCount) mTotalSize += 1;
-        else mTotalSize += fileSize;
-        mDownloadPool.execute(new DownloadTask(url, new File(mDestinationDirectory, relativePath), downloadHash));
+    /**
+     * Submits a download task for the given file.
+     *
+     * @param fileSize      the size of the file to be downloaded
+     * @param relativePath  the relative path of the file in the destination directory
+     * @param downloadHash  the SHA-1 hash of the file to be downloaded
+     * @param downloadUrls  the URLs of the file to be downloaded
+     */
+    public void submitDownload(int fileSize, String relativePath, @Nullable String downloadHash, String... downloadUrls) {
+        if (mUseFileCount) {
+            mTotalSize += 1;
+        } else {
+            mTotalSize += fileSize;
+        }
+        for (String downloadUrl : downloadUrls) {
+            mDownloadPool.submit(() -> downloadFile(fileSize, relativePath, downloadHash, downloadUrl));
+        }
     }
 
+    /**
+     * Submits a download task for the given file using a FileInfoProvider.
+     *
+     * @param infoProvider the FileInfoProvider for the file to be downloaded
+     */
     public void submitDownload(FileInfoProvider infoProvider) {
-        if(!mUseFileCount) throw new RuntimeException("This method can only be used in a file-counting ModDownloader");
+        if (!mUseFileCount) {
+            throw new RuntimeException("This method can only be used in a file-counting ModDownloader");
+        }
         mTotalSize += 1;
-        mDownloadPool.execute(new FileInfoQueryTask(infoProvider));
-    }
-
-    public void awaitFinish(Tools.DownloaderFeedback feedback) throws IOException {
-        try {
-            mDownloadPool.shutdown();
-            while(!mDownloadPool.awaitTermination(20, TimeUnit.MILLISECONDS) && !mTerminator.get()) {
-                feedback.updateProgress((int) mDownloadSize.get(), (int) mTotalSize);
-            }
-            if(mTerminator.get()) {
-                mDownloadPool.shutdownNow();
-                synchronized (mExceptionSyncPoint) {
-                    if(mFirstIOException == null) mExceptionSyncPoint.wait();
-                    throw mFirstIOException;
-                }
-            }
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static byte[] getThreadLocalBuffer() {
-        byte[] buffer = sThreadLocalBuffer.get();
-        if(buffer != null) return buffer;
-        buffer = new byte[8192];
-        sThreadLocalBuffer.set(buffer);
-        return buffer;
-    }
-
-    private void downloadFailed(IOException exception) {
-        mTerminator.set(true);
-        synchronized (mExceptionSyncPoint) {
-            if(mFirstIOException == null) {
-                mFirstIOException = exception;
-                mExceptionSyncPoint.notify();
-            }
-        }
-    }
-
-    class FileInfoQueryTask implements Runnable {
-        private final FileInfoProvider mFileInfoProvider;
-        public FileInfoQueryTask(FileInfoProvider fileInfoProvider) {
-            this.mFileInfoProvider = fileInfoProvider;
-        }
-        @Override
-        public void run() {
+        mDownloadPool.submit(() -> {
             try {
-                FileInfo fileInfo = mFileInfoProvider.getFileInfo();
-                if(fileInfo == null) return;
-                new DownloadTask(new String[]{fileInfo.url},
-                        new File(mDestinationDirectory, fileInfo.relativePath), fileInfo.sha1).run();
-            }catch (IOException e) {
+                FileInfo fileInfo = infoProvider.getFileInfo();
+                if (fileInfo == null) {
+                    return;
+                }
+                downloadFile(fileInfo.fileSize, fileInfo.relativePath, fileInfo.sha1, fileInfo.url);
+            } catch (IOException e) {
                 downloadFailed(e);
             }
+        });
+    }
+
+    /**
+     * Waits for all download tasks to complete and updates the progress.
+     *
+     * @param feedback the feedback object to update the progress
+     * @throws IOException if any download task fails
+     */
+    public void awaitFinish(Tools.DownloaderFeedback feedback) throws IOException {
+        try {
+            for (Future<?> future : mDownloadPool.shutdownNow()) {
+                future.get();
+            }
+            mDownloadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+            if (mTerminator.get()) {
+                throw mFirstIOException;
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IOException(e);
+        } catch (ExecutionException | TimeoutException e) {
+            throw new IOException(e.getCause());
         }
     }
 
-    class DownloadTask implements Runnable, Tools.DownloaderFeedback {
-        private final String[] mDownloadUrls;
-        private final File mDestination;
-        private final String mSha1;
-        private int last = 0;
-
-        public DownloadTask(String[] downloadurls,
-                            File downloadDestination, String downloadHash) {
-            this.mDownloadUrls = downloadurls;
-            this.mDestination = downloadDestination;
-            this.mSha1 = downloadHash;
-        }
-
-        @Override
-        public void run() {
-            for(String sourceUrl : mDownloadUrls) {
-                try {
-                    DownloadUtils.ensureSha1(mDestination, mSha1, (Callable<Void>) () -> {
-                        IOException exception = tryDownload(sourceUrl);
-                        if(exception != null) {
-                            throw exception;
-                        }
-                        return null;
-                    });
-
-                }catch (IOException e) {
+    /**
+     * Downloads the file from the given URL and saves it to the destination directory.
+     *
+     * @param fileSize      the size of the file to be downloaded
+     * @param relativePath  the relative path of the file in the destination directory
+     * @param downloadHash  the SHA-1 hash of the file to be downloaded
+     * @param downloadUrl   the URL of the file to be downloaded
+     */
+    private void downloadFile(int fileSize, String relativePath, @Nullable String downloadHash, String downloadUrl) {
+        for (int i = 0; i < DOWNLOAD_RETRY_COUNT; i++) {
+            try {
+                File destinationFile = new File(mDestinationDirectory, relativePath);
+                if (mUseFileCount) {
+                    DownloadUtils.downloadFile(downloadUrl, destinationFile);
+                } else {
+                    DownloadUtils.downloadFile(downloadUrl, destinationFile, fileSize);
+                }
+                if (downloadHash != null) {
+                    DownloadUtils.ensureSha1(destinationFile, downloadHash);
+                }
+                mDownloadSize.addAndGet(fileSize);
+                return;
+            } catch (IOException e) {
+                if (i == DOWNLOAD_RETRY_COUNT - 1) {
                     downloadFailed(e);
+                } else {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(DOWNLOAD_RETRY_DELAY_MILLIS);
+                    } catch (InterruptedException ignored) {
+                    }
                 }
             }
-        }
-
-        private IOException tryDownload(String sourceUrl) throws InterruptedException {
-            IOException exception = null;
-            for (int i = 0; i < 5; i++) {
-                try {
-                    DownloadUtils.downloadFileMonitored(sourceUrl, mDestination, getThreadLocalBuffer(), this);
-                    if(mUseFileCount) mDownloadSize.addAndGet(1);
-                    return null;
-                } catch (InterruptedIOException e) {
-                    throw new InterruptedException();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    exception = e;
-                }
-                if(!mUseFileCount) {
-                    mDownloadSize.addAndGet(-last);
-                    last = 0;
-                }
-            }
-            return exception;
-        }
-
-        @Override
-        public void updateProgress(int curr, int max) {
-            if(mUseFileCount) return;
-            mDownloadSize.addAndGet(curr - last);
-            last = curr;
         }
     }
 
+    /**
+     * Sets the first IO exception that occurred during download.
+     *
+     * @param exception the first IO exception that occurred during download
+     */
+    private void downloadFailed(IOException exception) {
+        synchronized (this) {
+            if (mFirstIOException == null) {
+                mFirstIOException = exception;
+            }
+        }
+        mTerminator.set(true);
+    }
+
+    /**
+     * A class representing the file information needed for downloading.
+     */
     public static class FileInfo {
-        public final String url;
+        public final int fileSize;
         public final String relativePath;
         public final String sha1;
+        public final String url;
 
-        public FileInfo(String url, String relativePath, @Nullable String sha1) {
-            this.url = url;
+        /**
+         * Constructs a new FileInfo instance with the given parameters.
+         *
+         * @param fileSize  the size of the file
+         * @param relativePath  the relative path of the file in the destination directory
+         * @param sha1  the SHA-1 hash of the file
+         * @param url  the URL of the file
+         */
+        public FileInfo(int fileSize, String relativePath, String sha1, String url) {
+            this.fileSize = fileSize;
             this.relativePath = relativePath;
             this.sha1 = sha1;
+            this.url = url;
         }
     }
 
+    /**
+     * A functional interface for providing file information.
+     */
+    @FunctionalInterface
     public interface FileInfoProvider {
         FileInfo getFileInfo() throws IOException;
     }
