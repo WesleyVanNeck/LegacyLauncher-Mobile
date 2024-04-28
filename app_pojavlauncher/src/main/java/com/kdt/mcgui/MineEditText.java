c9926397c63be6427f1e661391578db0eefed8cd
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:04 +0700

    improve

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/MineEditText.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/MineEditText.java
@@ -1,23 +1,29 @@
 package com.kdt.mcgui;
 
-import android.content.*;
-import android.util.*;
-import android.graphics.*;
-import android.widget.EditText;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.graphics.Color;
+import androidx.appcompat.widget.AppCompatEditText;
 
-public class MineEditText extends androidx.appcompat.widget.AppCompatEditText {
-	public MineEditText(Context ctx) {
-		super(ctx);
-		init();
-	}
+public class MineEditText extends AppCompatEditText {
 
-	public MineEditText(Context ctx, AttributeSet attrs) {
-		super(ctx, attrs);
-		init();
-	}
+    public MineEditText(Context context) {
+        super(context);
+        init();
+    }
 
-	public void init() {
-		setBackgroundColor(Color.parseColor("#131313"));
-		setPadding(5, 5, 5, 5);
-	}
+    public MineEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MineEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        setBackgroundColor(Color.parseColor("#131313"));
+        setPadding(5, 5, 5, 5);
+    }
 }
