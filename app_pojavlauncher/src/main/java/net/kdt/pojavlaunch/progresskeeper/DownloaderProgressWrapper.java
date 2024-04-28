f0f521a23796bf6ec19d138ae7b40d65bae973e2
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:24 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/progresskeeper/DownloaderProgressWrapper.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/progresskeeper/DownloaderProgressWrapper.java
@@ -10,31 +10,43 @@ public class DownloaderProgressWrapper implements Tools.DownloaderFeedback {
     private final String mProgressRecord;
     public String extraString = null;
 
+    public static final long BYTE_TO_MB = 1024 * 1024;
+
     /**
      * A simple wrapper to send the downloader progress to ProgressKeeper
      * @param progressString the string that will be used in the progress reporter
      * @param progressRecord the record for ProgressKeeper
      */
     public DownloaderProgressWrapper(int progressString, String progressRecord) {
+        this(progressString, progressRecord, null);
+    }
+
+    public DownloaderProgressWrapper(int progressString, String progressRecord, String extraString) {
         this.mProgressString = progressString;
         this.mProgressRecord = progressRecord;
+        this.extraString = extraString;
     }
 
     @Override
-    public void updateProgress(int curr, int max) {
+    public void updateProgress(long curr, long max) {
         Object[] va;
-        if(extraString != null)  {
+        if (extraString != null) {
             va = new Object[3];
             va[0] = extraString;
-            va[1] = curr/BYTE_TO_MB;
-            va[2] = max/BYTE_TO_MB;
-        }
-        else {
+            va[1] = curr / BYTE_TO_MB;
+            va[2] = max / BYTE_TO_MB;
+        } else {
             va = new Object[2];
-            va[0] = curr/BYTE_TO_MB;
-            va[1] = max/BYTE_TO_MB;
+            va[0] = curr / BYTE_TO_MB;
+            va[1] = max / BYTE_TO_MB;
         }
-        // the allocations are fine because thats how java implements variadic arguments in bytecode: an array of whatever
-        ProgressKeeper.submitProgress(mProgressRecord, (int) Math.max((float)curr/max*100,0), mProgressString, va);
+
+        String message = java.text.MessageFormat.format(
+                "{0} - {1}/{2} MB",
+                mProgressRecord,
+                va[0],
+                va[1]);
+
+        ProgressKeeper.submitProgress(mProgressString, (int) Math.max((float) curr / max * 100, 0), message);
     }
 }
