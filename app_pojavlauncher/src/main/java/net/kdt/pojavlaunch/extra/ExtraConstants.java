a2f0def98fd57851221e1c2029e3650ede8a5e4c
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:13 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/extra/ExtraConstants.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/extra/ExtraConstants.java
@@ -1,25 +1,63 @@
+// net.kdt.pojavlaunch.extra
+
 package net.kdt.pojavlaunch.extra;
 
-public class ExtraConstants {
-    /* ExtraCore constant: a HashMap for converting values such as latest-snapshot or latest-release to actual game version names */
+/**
+ * A class containing various constants used throughout the ExtraCore module.
+ */
+public final class ExtraConstants {
+
+    /**
+     * A mapping of game version names to their corresponding display names.
+     */
     public static final String RELEASE_TABLE = "release_table";
-    /* ExtraCore constant: Serpent's back button tracking thing */
+
+    /**
+     * A preference key used to track whether the back button has been pressed.
+     */
     public static final String BACK_PREFERENCE = "back_preference";
-    /* ExtraCore constant: The OPENGL version that should be exposed */
+
+    /**
+     * The OpenGL version to expose.
+     */
     public static final String OPEN_GL_VERSION = "open_gl_version";
-    /* ExtraCore constant: When the microsoft authentication via webview is done */
+
+    /**
+     * A preference key used to indicate when the Microsoft authentication via webview is done.
+     */
     public static final String MICROSOFT_LOGIN_TODO = "webview_login_done";
-    /* ExtraCore constant: Mojang or "local" authentication to perform */
+
+    /**
+     * A preference key used to indicate whether to perform Mojang or "local" authentication.
+     */
     public static final String MOJANG_LOGIN_TODO = "mojang_login_todo";
-    /* ExtraCore constant: Add minecraft account procedure, the user has to select between mojang or microsoft */
+
+    /**
+     * A preference key used to trigger the account selection procedure.
+     */
     public static final String SELECT_AUTH_METHOD = "start_login_procedure";
-    /* ExtraCore constant: Selected file or folder, as a String */
+
+    /**
+     * A preference key used to store the selected file or folder as a string.
+     */
     public static final String FILE_SELECTOR = "file_selector";
-    /* ExtraCore constant: Need to refresh the version spinner, selecting the uuid at the same time. Can be DELETED_PROFILE */
+
+    /**
+     * A preference key used to indicate that the version spinner needs to be refreshed.
+     */
     public static final String REFRESH_VERSION_SPINNER = "refresh_version";
-    /* ExtraCore Constant: When we want to launch the game */
+
+    /**
+     * A preference key used to trigger the game launch.
+     */
     public static final String LAUNCH_GAME = "launch_game";
 
-
-
+    // Prevent instantiation
+    private ExtraConstants() {
+        throw new AssertionError("This class cannot be instantiated.");
+    }
 }
+
+/* JVM
+version=11
+*/
