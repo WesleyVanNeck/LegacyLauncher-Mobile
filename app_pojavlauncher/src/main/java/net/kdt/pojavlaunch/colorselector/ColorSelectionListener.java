2b8a8b49bcb98761ff7d2a9a56a800d22252af9d
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:08 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/colorselector/ColorSelectionListener.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/colorselector/ColorSelectionListener.java
@@ -1,5 +1,6 @@
 package net.kdt.pojavlaunch.colorselector;
 
+@FunctionalInterface
 public interface ColorSelectionListener {
     /**
      * This method gets called by the ColorSelector when the color is selected
@@ -7,3 +8,9 @@ public interface ColorSelectionListener {
      */
     void onColorSelected(int color);
 }
+
+// Usage example:
+ColorSelectionListener listener = (color) -> {
+    // Do something with the selected color...
+    System.out.println("Selected color: #" + Integer.toHexString(color));
+};
