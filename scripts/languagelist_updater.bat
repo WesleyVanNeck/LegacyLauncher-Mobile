dd294722f2242a9eef0f9de68320d5c8c1bb576f
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:36 +0700

    improve

--- scripts/languagelist_updater.bat
+++ scripts/languagelist_updater.bat
@@ -1,8 +1,11 @@
 @echo off
+setlocal
 
-set thisdir = "%~dp0"
-set langfile = %thisdir%\..\app_pojavlauncher\src\main\assets\language_list.txt
+set "thisdir=%~dp0"
+set "langfile=%thisdir%\..\app_pojavlauncher\src\main\assets\language_list.txt"
 
-del %langfile%
-dir %thisdir%\..\app_pojavlauncher\src\main\res\values-* /s /b > %langfile%
+if exist "%langfile%" del /q "%langfile%"
 
+dir /s /b "%thisdir%\..\app_pojavlauncher\src\main\res\values-*" > "%langfile%"
+
+endlocal
