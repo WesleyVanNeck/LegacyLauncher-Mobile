5cd15ba6ad70dbe9301c2bf77fb948d50555a546
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:10 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/handleview/ActionButtonInterface.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/handleview/ActionButtonInterface.java
@@ -4,21 +4,40 @@ import android.view.View;
 
 import net.kdt.pojavlaunch.customcontrols.buttons.ControlInterface;
 
-/** Interface defining the behavior of action buttons */
+/**
+ * Interface defining the behavior of action buttons.
+ */
 public interface ActionButtonInterface extends View.OnClickListener {
 
-    /** HAS TO BE CALLED BY THE CONSTRUCTOR */
+    /**
+     * This method should be implemented by concrete classes to initialize any required state.
+     */
     void init();
 
-    /** Called when the button should be made aware of the current target */
+    /**
+     * Called when the button should be made aware of the current target.
+     *
+     * @param view the target view
+     */
     void setFollowedView(ControlInterface view);
 
-    /** Called when the button action should be executed on the target */
+    /**
+     * Called when the button action should be executed on the target.
+     */
     void onClick();
 
-    /** Whether the button should be shown, given the current contextual information that it has */
+    /**
+     * Whether the button should be shown, given the current contextual information that it has.
+     *
+     * @return true if the button should be visible, false otherwise
+     */
     boolean shouldBeVisible();
 
-    @Override  // Wrapper to remove the arg
-    default void onClick(View v){onClick();}
+    /**
+     * Wrapper to remove the arg.
+     */
+    @Override
+    default void onClick(View v) {
+        onClick();
+    }
 }
