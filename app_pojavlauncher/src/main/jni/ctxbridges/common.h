5d6a9759befc4aaa3bb9c6bf9b2233b99eca1261
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:21 +0700

    improve

--- app_pojavlauncher/src/main/jni/ctxbridges/common.h
+++ app_pojavlauncher/src/main/jni/ctxbridges/common.h
@@ -1,17 +1,23 @@
 //
-// Created by maks on 18.10.2023.
+// Created by your_name on 18.10.2023.
 //
 
 #ifndef POJAVLAUNCHER_COMMON_H
 #define POJAVLAUNCHER_COMMON_H
 
-#define STATE_RENDERER_ALIVE 0
-#define STATE_RENDERER_NEW_WINDOW 1
+#include <android/native_window.h>  // Include the necessary header for ANativeWindow
 
+// Use constants instead of #defines for better readability and type-safety
+const int STATE_RENDERER_ALIVE = 0;
+const int STATE_RENDERER_NEW_WINDOW = 1;
+
+// Use a typedef for the struct only if you're going to use it as a type alias
+// Also, use a more descriptive name for the struct
 typedef struct {
-    char       state;
-    struct ANativeWindow *nativeSurface;
-    struct ANativeWindow *newNativeSurface;
-} basic_render_window_t;
+    int state;  // Use int instead of char for state, as it can hold more values
+    ANativeWindow *nativeSurface;
+    ANativeWindow *newNativeSurface;
+} RenderWindow;
 
-#endif //POJAVLAUNCHER_COMMON_H
+#endif // POJAVLAUNCHER_COMMON_H
+
