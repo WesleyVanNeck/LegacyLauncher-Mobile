e0866f5798ff13d7ced7a9f93002f88f8e499772
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:18 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/value/launcherprofiles/MinecraftResolution.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/value/launcherprofiles/MinecraftResolution.java
@@ -4,6 +4,29 @@ import androidx.annotation.Keep;
 
 @Keep
 public class MinecraftResolution {
-	public int width;
-	public int height;
+    private int width;
+    private int height;
+
+    public MinecraftResolution() {}
+
+    public MinecraftResolution(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
