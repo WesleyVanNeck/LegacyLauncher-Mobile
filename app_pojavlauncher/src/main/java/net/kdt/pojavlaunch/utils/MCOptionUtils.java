5a6a9ff5768e8d056b70212fb0e8b0d39d3e68c1
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:31 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/MCOptionUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/MCOptionUtils.java
@@ -1,4 +1,5 @@
 package net.kdt.pojavlaunch.utils;
+
 import static org.lwjgl.glfw.CallbackBridge.windowHeight;
 import static org.lwjgl.glfw.CallbackBridge.windowWidth;
 
@@ -9,175 +10,199 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import net.kdt.pojavlaunch.Tools;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 public class MCOptionUtils {
-    private static final HashMap<String,String> sParameterMap = new HashMap<>();
-    private static final ArrayList<WeakReference<MCOptionListener>> sOptionListeners = new ArrayList<>();
-    private static FileObserver sFileObserver;
-    private static String sOptionFolderPath = null;
+    private static final String OPTIONS_FILE = "options.txt";
+    private static final Map<String, String> PARAMETER_MAP = new WeakHashMap<>();
+    private static FileObserver FILE_OBSERVER;
+    private static String OPTION_FOLDER_PATH = null;
+
     public interface MCOptionListener {
-        /** Called when an option is changed. Don't know which one though */
+        /**
+         * Called when an option is changed.
+         * Don't know which one though
+         */
         void onOptionChanged();
     }
 
-
-    public static void load(){
-        load(sOptionFolderPath == null
+    public static void load() {
+        load(OPTION_FOLDER_PATH == null
                 ? Tools.DIR_GAME_NEW
-                : sOptionFolderPath);
+                : OPTION_FOLDER_PATH);
     }
 
     public static void load(@NonNull String folderPath) {
-        File optionFile = new File(folderPath + "/options.txt");
-        if(!optionFile.exists()) {
-            try { // Needed for new instances I guess  :think:
+        File optionFile = new File(folderPath, OPTIONS_FILE);
+
+        if (!optionFile.exists()) {
+            try {
                 optionFile.createNewFile();
-            } catch (IOException e) { e.printStackTrace(); }
+            } catch (IOException e) {
+                Log.w(Tools.APP_NAME, "Could not create options.txt", e);
+            }
         }
 
-        if(sFileObserver == null || !Objects.equals(sOptionFolderPath, folderPath)){
-            sOptionFolderPath = folderPath;
+        if (FILE_OBSERVER == null || !Objects.equals(OPTION_FOLDER_PATH, folderPath)) {
+            stopFileObserver();
+            OPTION_FOLDER_PATH = folderPath;
             setupFileObserver();
         }
-        sOptionFolderPath = folderPath; // Yeah I know, it may be redundant
 
-        sParameterMap.clear();
+        PARAMETER_MAP.clear();
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(optionFile));
+        try (BufferedReader reader = new BufferedReader(new FileReader(optionFile))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 int firstColonIndex = line.indexOf(':');
-                if(firstColonIndex < 0) {
-                    Log.w(Tools.APP_NAME, "No colon on line \""+line+"\", skipping");
+                if (firstColonIndex < 0) {
+                    Log.w(Tools.APP_NAME, "No colon on line \"" + line + "\", skipping");
                     continue;
                 }
-                sParameterMap.put(line.substring(0,firstColonIndex), line.substring(firstColonIndex+1));
+                PARAMETER_MAP.put(line.substring(0, firstColonIndex), line.substring(firstColonIndex + 1));
             }
-            reader.close();
         } catch (IOException e) {
             Log.w(Tools.APP_NAME, "Could not load options.txt", e);
         }
     }
 
-    public static void set(String key, String value) {
-        sParameterMap.put(key,value);
+    public static void set(@NonNull String key, @Nullable String value) {
+        if (value == null) {
+            PARAMETER_MAP.remove(key);
+        } else {
+            PARAMETER_MAP.put(key, value);
+        }
     }
 
-    /** Set an array of String, instead of a simple value. Not supported on all options */
-    public static void set(String key, List<String> values){
-        sParameterMap.put(key, values.toString());
+    public static String get(@NonNull String key) {
+        return PARAMETER_MAP.get(key);
     }
 
-    public static String get(String key){
-        return sParameterMap.get(key);
-    }
-
-    /** @return A list of values from an array stored as a string */
-    public static List<String> getAsList(String key){
+    public static List<String> getAsList(@NonNull String key) {
         String value = get(key);
 
-        // Fallback if the value doesn't exist
-        if (value == null) return new ArrayList<>();
+        if (value == null) {
+            return new ArrayList<>();
+        }
 
         // Remove the edges
         value = value.replace("[", "").replace("]", "");
-        if (value.isEmpty()) return new ArrayList<>();
+        if (value.isEmpty()) {
+            return new ArrayList<>();
+        }
 
         return Arrays.asList(value.split(","));
     }
 
     public static void save() {
         StringBuilder result = new StringBuilder();
-        for(String key : sParameterMap.keySet())
+        for (String key : PARAMETER_MAP.keySet()) {
             result.append(key)
                     .append(':')
-                    .append(sParameterMap.get(key))
+                    .append(PARAMETER_MAP.get(key))
                     .append('\n');
+        }
 
-        try {
-            sFileObserver.stopWatching();
-            Tools.write(sOptionFolderPath + "/options.txt", result.toString());
-            sFileObserver.startWatching();
+        File optionFile = new File(OPTION_FOLDER_PATH, OPTIONS_FILE);
+
+        try (FileWriter writer = new FileWriter(optionFile)) {
+            writer.write(result.toString());
         } catch (IOException e) {
             Log.w(Tools.APP_NAME, "Could not save options.txt", e);
         }
     }
 
-    /** @return The stored Minecraft GUI scale, also auto-computed if on auto-mode or improper setting */
+    /**
+     * @return The stored Minecraft GUI scale, also auto-computed if on auto-mode or improper setting
+     */
     public static int getMcScale() {
-        String str = MCOptionUtils.get("guiScale");
-        int guiScale = (str == null ? 0 :Integer.parseInt(str));
+        String str = get("guiScale");
+        int guiScale = (str == null ? 0 : Integer.parseInt(str));
 
         int scale = Math.max(Math.min(windowWidth / 320, windowHeight / 240), 1);
-        if(scale < guiScale || guiScale == 0){
+        if (scale < guiScale || guiScale == 0) {
             guiScale = scale;
         }
 
         return guiScale;
     }
 
-    /** Add a file observer to reload options on file change
-     * Listeners get notified of the change */
-    private static void setupFileObserver(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
-            sFileObserver = new FileObserver(new File(sOptionFolderPath + "/options.txt"), FileObserver.MODIFY) {
+    /**
+     * Add a file observer to reload options on file change
+     * Listeners get notified of the change
+     */
+    private static void setupFileObserver() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            FILE_OBSERVER = new FileObserver(new File(OPTION_FOLDER_PATH, OPTIONS_FILE), FileObserver.MODIFY) {
                 @Override
                 public void onEvent(int i, @Nullable String s) {
-                    MCOptionUtils.load();
+                    load();
                     notifyListeners();
                 }
             };
-        }else{
-            sFileObserver = new FileObserver(sOptionFolderPath + "/options.txt", FileObserver.MODIFY) {
+        } else {
+            FILE_OBSERVER = new FileObserver(OPTION_FOLDER_PATH + "/" + OPTIONS_FILE, FileObserver.MODIFY) {
                 @Override
                 public void onEvent(int i, @Nullable String s) {
-                    MCOptionUtils.load();
+                    load();
                     notifyListeners();
                 }
             };
         }
 
-        sFileObserver.startWatching();
+        FILE_OBSERVER.startWatching();
     }
 
-    /** Notify the option listeners */
-    public static void notifyListeners(){
-        for(WeakReference<MCOptionListener> weakReference : sOptionListeners){
+    /**
+     * Notify the option listeners
+     */
+    public static void notifyListeners() {
+        for (WeakReference<MCOptionListener> weakReference : sOptionListeners) {
             MCOptionListener optionListener = weakReference.get();
-            if(optionListener == null) continue;
+            if (optionListener == null) continue;
 
             optionListener.onOptionChanged();
         }
     }
 
-    /** Add an option listener, notice how we don't have a reference to it */
-    public static void addMCOptionListener(MCOptionListener listener){
+    /**
+     * Add an option listener, notice how we don't have a reference to it
+     */
+    public static void addMCOptionListener(@NonNull MCOptionListener listener) {
         sOptionListeners.add(new WeakReference<>(listener));
     }
 
-    /** Remove a listener from existence, or at least, its reference here */
-    public static void removeMCOptionListener(MCOptionListener listener){
-        for(WeakReference<MCOptionListener> weakReference : sOptionListeners){
+    /**
+     * Remove a listener from existence, or at least, its reference here
+     */
+    public static void removeMCOptionListener(@NonNull MCOptionListener listener) {
+        for (WeakReference<MCOptionListener> weakReference : sOptionListeners) {
             MCOptionListener optionListener = weakReference.get();
-            if(optionListener == null) continue;
-            if(optionListener == listener){
+            if (optionListener == null) continue;
+            if (optionListener == listener) {
                 sOptionListeners.remove(weakReference);
                 return;
             }
         }
     }
 
+    private static void stopFileObserver() {
+        if (FILE_OBSERVER != null) {
+            FILE_OBSERVER.stopWatching();
+            FILE_OBSERVER = null;
+        }
+    }
+
+    private static final List<WeakReference<MCOptionListener>> sOptionListeners = new ArrayList<>();
 }
