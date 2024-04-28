8edb83366ce56af3b904918f93ffbcc731caf2f2
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:21 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/extra/ExtraListener.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/extra/ExtraListener.java
@@ -1,20 +1,87 @@
 package net.kdt.pojavlaunch.extra;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.function.BiConsumer;
 
 /**
- * Listener class for the ExtraCore
- * An ExtraListener can listen to a virtually unlimited amount of values
+ * Listener interface for the ExtraCore.
+ * Allows listening to a virtually unlimited amount of values.
  */
 public interface ExtraListener<T> {
 
     /**
-     * Called upon a new value being set
-     * @param key The name of the value
-     * @param value The new value as a string
-     * @return Whether you consume the Listener (stop listening)
+     * Called when a new value is set for the given key.
+     *
+     * @param key   The name of the value.
+     * @param value The new value as an object.
+     * @return Whether the listener consumed the event (stopped listening).
      */
-    @SuppressWarnings("SameReturnValue")
     boolean onValueSet(String key, @NonNull T value);
 
+    /**
+     * Convenience method to create a new ExtraListener instance that simply calls the given
+     * BiConsumer when a new value is set.
+     *
+     * @param consumer The BiConsumer to call when a new value is set.
+     * @param <V>      The type of the value.
+     * @return A new ExtraListener instance.
+     */
+    static <V> ExtraListener<V> fromBiConsumer(BiConsumer<String, V> consumer) {
+        return new ExtraListener<V>() {
+            @Override
+            public boolean onValueSet(String key, @NonNull V value) {
+                consumer.accept(key, value);
+                return false;
+            }
+        };
+    }
+
+    /**
+     * Convenience method to create a new ExtraListener instance that simply calls the given
+     * method when a new value is set.
+     *
+     * @param methodName The name of the method to call when a new value is set.
+     * @param obj        The object to call the method on.
+     * @param <V>        The type of the value.
+     * @return A new ExtraListener instance.
+     */
+    static <V> ExtraListener<V> fromMethod(String methodName, Object obj) {
+        return new ExtraListener<V>() {
+            @Override
+            public boolean onValueSet(String key, @NonNull V value) {
+                try {
+                    obj.getClass().getMethod(methodName, String.class, V.class).invoke(obj, key, value);
+                } catch (Exception e) {
+                    throw new RuntimeException("Failed to invoke method " + methodName, e);
+                }
+                return false;
+            }
+        };
+    }
+
+    /**
+     * Convenience method to create a new ExtraListener instance that simply calls the given
+     * method when a new value is set, and returns the result of the method as the consumed value.
+     *
+     * @param methodName The name of the method to call when a new value is set.
+     * @param obj        The object to call the method on.
+     * @param <V>        The type of the value.
+     * @param <R>        The type of the result.
+     * @return A new ExtraListener instance.
+     */
+    static <V, R> ExtraListener<V> fromConsumingMethod(String methodName, Object obj) {
+        return new ExtraListener<V>() {
+            @Override
+            public boolean onValueSet(String key, @NonNull V value) {
+                try {
+                    R result = (R) obj.getClass().getMethod(methodName, String.class, V.class).invoke(obj, key, value);
+                    return true; // consumed
+                } catch (Exception e) {
+                    throw new RuntimeException("Failed to invoke method " + methodName, e);
+                }
+            }
+        };
+    }
 }
