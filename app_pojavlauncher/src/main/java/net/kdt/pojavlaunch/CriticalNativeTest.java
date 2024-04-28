81c25d75350378c873e13f89d5e8ee4609518ce4
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:09 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/CriticalNativeTest.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/CriticalNativeTest.java
@@ -1,11 +1,33 @@
 package net.kdt.pojavlaunch;
 
-import dalvik.annotation.optimization.CriticalNative;
+import android.annotation.SuppressLint;
 
 public class CriticalNativeTest {
+
+    /**
+     * A native method that will be implemented in the native code.
+     * This method is marked as critical, indicating that it should be optimized for performance.
+     *
+     * @param arg0 The first argument.
+     * @param arg1 The second argument.
+     */
+    @SuppressLint("NewApi")
     @CriticalNative
     public static native void testCriticalNative(int arg0, int arg1);
+
+    /**
+     * Invokes the native method.
+     */
     public static void invokeTest() {
         testCriticalNative(0, 0);
+    }
+
+    /**
+     * A sample main method to demonstrate how to use this class.
+     *
+     * @param args The command line arguments.
+     */
+    public static void main(String[] args) {
+        invokeTest();
     }
 }
