ddadb30f4828c88ba3856dc43cb04cf8cad9facc
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:16 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/multirt/RTSpinnerAdapter.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/multirt/RTSpinnerAdapter.java
@@ -1,7 +1,6 @@
 package net.kdt.pojavlaunch.multirt;
 
 import android.content.Context;
-import android.database.DataSetObserver;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,18 +17,28 @@ import java.util.List;
 public class RTSpinnerAdapter implements SpinnerAdapter {
     final Context mContext;
     final List<Runtime> mRuntimes;
-    public RTSpinnerAdapter(@NonNull Context context, List<Runtime> runtimes) {
+
+    public RTSpinnerAdapter(@NonNull Context context, @NonNull List<Runtime> runtimes) {
         mRuntimes = runtimes;
-        Runtime runtime = new Runtime("<Default>", "", null, 0);
-        mRuntimes.add(runtime);
+        Runtime defaultRuntime = new Runtime("<Default>", "", null, 0);
+        mRuntimes.add(defaultRuntime);
         mContext = context;
     }
 
     @Override
-    public void registerDataSetObserver(DataSetObserver observer) {}
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
+    }
 
     @Override
-    public void unregisterDataSetObserver(DataSetObserver observer) {}
+    public boolean areAllItemsEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return true;
+    }
 
     @Override
     public int getCount() {
@@ -37,7 +46,7 @@ public class RTSpinnerAdapter implements SpinnerAdapter {
     }
 
     @Override
-    public Object getItem(int position) {
+    public Runtime getItem(int position) {
         return mRuntimes.get(position);
     }
 
@@ -54,17 +63,22 @@ public class RTSpinnerAdapter implements SpinnerAdapter {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View view = convertView != null?
-                convertView:
-                LayoutInflater.from(mContext).inflate(R.layout.item_simple_list_1, parent,false);
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.item_simple_list_1, parent, false);
+        } else {
+            view = convertView;
+        }
+        view.setTag(position);
 
         Runtime runtime = mRuntimes.get(position);
-        if(position == mRuntimes.size() - 1 ){
-            ((TextView) view).setText(runtime.name);
-        }else{
-            ((TextView) view).setText(String.format("%s - %s",
+        TextView textView = view.findViewById(R.id.textView);
+        if (position == mRuntimes.size() - 1) {
+            textView.setText(runtime.name);
+        } else {
+            textView.setText(String.format("%s - %s",
                     runtime.name.replace(".tar.xz", ""),
-                    runtime.versionString == null ? view.getResources().getString(R.string.multirt_runtime_corrupt) : runtime.versionString));
+                    runtime.versionString == null ? parent.getResources().getString(R.string.multirt_runtime_corrupt) : runtime.versionString));
         }
 
         return view;
@@ -73,21 +87,4 @@ public class RTSpinnerAdapter implements SpinnerAdapter {
     @Override
     public int getItemViewType(int position) {
         return 0;
-    }
 
-    @Override
-    public int getViewTypeCount() {
-        return 1;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return mRuntimes.isEmpty();
-    }
-
-    @Override
-    public View getDropDownView(int position, View convertView, ViewGroup parent) {
-        return getView(position,convertView,parent);
-    }
-
-}
