8896d733c258eb7d6bf6007374f2fc1952a50312
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:15 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/ControlDrawerData.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/ControlDrawerData.java
@@ -1,23 +1,34 @@
 package net.kdt.pojavlaunch.customcontrols;
 
-import net.kdt.pojavlaunch.Tools;
+import androidx.annotation.Keep;
 
 import java.util.ArrayList;
 
-import static net.kdt.pojavlaunch.customcontrols.ControlDrawerData.Orientation.DOWN;
-import static net.kdt.pojavlaunch.customcontrols.ControlDrawerData.Orientation.LEFT;
-import static net.kdt.pojavlaunch.customcontrols.ControlDrawerData.Orientation.RIGHT;
-import static net.kdt.pojavlaunch.customcontrols.ControlDrawerData.Orientation.UP;
-import static net.kdt.pojavlaunch.customcontrols.ControlDrawerData.Orientation.FREE;
-
-@androidx.annotation.Keep
+/**
+ * Data class representing the properties of a control drawer.
+ */
+@Keep
 public class ControlDrawerData {
 
+    /**
+     * List of button properties.
+     */
     public final ArrayList<ControlData> buttonProperties;
+
+    /**
+     * Properties of the control drawer.
+     */
     public final ControlData properties;
+
+    /**
+     * Orientation of the control drawer.
+     */
     public Orientation orientation;
 
-    @androidx.annotation.Keep
+    /**
+     * Enum representing the possible orientations of the control drawer.
+     */
+    @Keep
     public enum Orientation {
         DOWN,
         LEFT,
@@ -26,58 +37,80 @@ public class ControlDrawerData {
         FREE
     }
 
-    public static Orientation[] getOrientations(){
-        return new Orientation[]{DOWN,LEFT,UP,RIGHT,FREE};
+    /**
+     * Gets an array of all possible orientations.
+     *
+     * @return an array of all possible orientations
+     */
+    public static Orientation[] getOrientations() {
+        return new Orientation[]{Orientation.DOWN, Orientation.LEFT, Orientation.UP, Orientation.RIGHT, Orientation.FREE};
     }
 
-    public static int orientationToInt(Orientation orientation){
-        switch (orientation){
-            case DOWN: return 0;
-            case LEFT: return 1;
-            case UP: return 2;
-            case RIGHT: return 3;
-            case FREE: return 4;
+    /**
+     * Converts an orientation to an integer.
+     *
+     * @param orientation the orientation to convert
+     * @return the integer representation of the orientation
+     */
+    public static int orientationToInt(Orientation orientation) {
+        switch (orientation) {
+            case DOWN:
+                return 0;
+            case LEFT:
+                return 1;
+            case UP:
+                return 2;
+            case RIGHT:
+                return 3;
+            case FREE:
+                return 4;
+            default:
+                throw new IllegalArgumentException("Invalid orientation: " + orientation);
         }
-        return -1;
     }
 
-    public static Orientation intToOrientation(int by){
-        switch (by){
-            case 0: return DOWN;
-            case 1: return LEFT;
-            case 2: return UP;
-            case 3: return RIGHT;
-            case 4: return FREE;
+    /**
+     * Converts an integer to an orientation.
+     *
+     * @param by the integer to convert
+     * @return the orientation representation of the integer
+     */
+    public static Orientation intToOrientation(int by) {
+        switch (by) {
+            case 0:
+                return Orientation.DOWN;
+            case 1:
+                return Orientation.LEFT;
+            case 2:
+                return Orientation.UP;
+            case 3:
+                return Orientation.RIGHT;
+            case 4:
+                return Orientation.FREE;
+            default:
+                throw new IllegalArgumentException("Invalid integer: " + by);
         }
-        return null;
     }
 
-    public ControlDrawerData(){
-        this(new ArrayList<>());
+    /**
+     * Constructor for ControlDrawerData.
+     *
+     * @param buttonProperties list of button properties
+     * @param properties       properties of the control drawer
+     * @param orientation      orientation of the control drawer
+     */
+    public ControlDrawerData(ArrayList<ControlData> buttonProperties, ControlData properties, Orientation orientation) {
+        this.buttonProperties = buttonProperties != null ? buttonProperties : new ArrayList<>();
+        this.properties = properties != null ? properties : new ControlData("Drawer", new int[0], 0, 0);
+        this.orientation = orientation != null ? orientation : Orientation.LEFT;
     }
 
-    public ControlDrawerData(ArrayList<ControlData> buttonProperties){
-        this(buttonProperties, new ControlData("Drawer", new int[] {}, Tools.currentDisplayMetrics.widthPixels/2f, Tools.currentDisplayMetrics.heightPixels/2f));
+    /**
+     * Copy constructor for ControlDrawerData.
+     *
+     * @param drawerData the ControlDrawerData object to copy
+     */
+    public ControlDrawerData(ControlDrawerData drawerData) {
+        this(drawerData.buttonProperties, drawerData.properties, drawerData.orientation);
     }
-
-    public ControlDrawerData(ArrayList<ControlData> buttonProperties, ControlData properties){
-        this(buttonProperties, properties, Orientation.LEFT);
-    }
-
-
-    public ControlDrawerData(ArrayList<ControlData> buttonProperties, ControlData properties, Orientation orientation){
-        this.buttonProperties = buttonProperties;
-        this.properties = properties;
-        this.orientation = orientation;
-    }
-
-    public ControlDrawerData(ControlDrawerData drawerData){
-        buttonProperties = new ArrayList<>(drawerData.buttonProperties.size());
-        for(ControlData controlData : drawerData.buttonProperties){
-            buttonProperties.add(new ControlData(controlData));
-        }
-        properties = new ControlData(drawerData.properties);
-        orientation = drawerData.orientation;
-    }
-
 }
