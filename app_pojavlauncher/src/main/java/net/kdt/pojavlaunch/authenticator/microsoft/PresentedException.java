b86486cd33e120317e0c8039ba8a2bad8e5dccab
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:10 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/authenticator/microsoft/PresentedException.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/authenticator/microsoft/PresentedException.java
@@ -2,22 +2,42 @@ package net.kdt.pojavlaunch.authenticator.microsoft;
 
 import android.content.Context;
 
-public class PresentedException extends RuntimeException {
-    final int localizationStringId;
-    final Object[] extraArgs;
+public final class PresentedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private final int localizationStringId;
+    private final Object[] extraArgs;
 
     public PresentedException(int localizationStringId, Object... extraArgs) {
+        super();
         this.localizationStringId = localizationStringId;
         this.extraArgs = extraArgs;
     }
 
-    public PresentedException(Throwable throwable, int localizationStringId, Object... extraArgs) {
-        super(throwable);
+    public PresentedException(Throwable cause, int localizationStringId, Object... extraArgs) {
+        super(cause);
         this.localizationStringId = localizationStringId;
         this.extraArgs = extraArgs;
     }
 
-    public String toString(Context context) {
+    public String getLocalizedMessage(Context context) {
         return context.getString(localizationStringId, extraArgs);
+    }
+
+    @Override
+    public String getMessage() {
+        return getLocalizedMessage(getContext());
+    }
+
+    private Context getContext() {
+        Throwable cause = getCause();
+        while (cause != null) {
+            if (cause instanceof Context) {
+                return (Context) cause;
+            }
+            cause = cause.getCause();
+        }
+        throw new IllegalStateException("No Context available to get localized message.");
     }
 }
