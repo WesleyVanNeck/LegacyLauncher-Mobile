3a17c8677672c8c1135689af75519dc53dbeba42
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:12 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/models/ModItem.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/models/ModItem.java
@@ -2,36 +2,17 @@ package net.kdt.pojavlaunch.modloaders.modpacks.models;
 
 import androidx.annotation.NonNull;
 
-public class ModItem extends ModSource {
+public class ModItem implements ModSource {
 
-    public String id;
-    public String title;
-    public String description;
-    public String imageUrl;
-
-    public ModItem(int apiSource, boolean isModpack, String id, String title, String description, String imageUrl) {
-        this.apiSource = apiSource;
-        this.isModpack = isModpack;
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.imageUrl = imageUrl;
-    }
+    private static final String CACHE_TAG_DELIMITER = "_";
 
     @NonNull
-    @Override
-    public String toString() {
-        return "ModItem{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", apiSource=" + apiSource +
-                ", isModpack=" + isModpack +
-                '}';
-    }
+    private final int apiSource;
+    @NonNull
+    private final boolean isModpack;
+    @NonNull
+    private final String id;
+    @NonNull
+    private final String title;
+    @NonNull
 
-    public String getIconCacheTag() {
-        return apiSource+"_"+id;
-    }
-}
