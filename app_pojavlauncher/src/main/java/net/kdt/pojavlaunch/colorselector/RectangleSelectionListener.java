a8d19ad342ebb36dd26151daf58933fb5cc0fc99
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:10 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/colorselector/RectangleSelectionListener.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/colorselector/RectangleSelectionListener.java
@@ -1,5 +1,25 @@
 package net.kdt.pojavlaunch.colorselector;
 
+import java.util.function.BiConsumer;
+
 public interface RectangleSelectionListener {
-    void onLuminosityIntensityChanged(float luminosity, float intensity);
+    default void onLuminosityIntensityChanged(float luminosity, float intensity) {
+        // Provide a default implementation here, if needed
+    }
+
+    void setOnLuminosityIntensityChanged(BiConsumer<Float, Float> listener);
 }
+
+
+RectangleSelectionListener listener = ...;
+listener.setOnLuminosityIntensityChanged((luminosity, intensity) -> {
+    // Do something when the luminosity and intensity values change
+});
+
+
+RectangleSelectionListener listener = new RectangleSelectionListener() {
+    @Override
+    public void onLuminosityIntensityChanged(float luminosity, float intensity) {
+        // Provide custom behavior here
+    }
+};
