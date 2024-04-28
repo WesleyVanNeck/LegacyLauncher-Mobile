e4c256d7ab1d0f7d03157f6010dfcc3861cebf76
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:11 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/models/ModSource.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/models/ModSource.java
@@ -1,6 +1,30 @@
 package net.kdt.pojavlaunch.modloaders.modpacks.models;
 
 public abstract class ModSource {
-    public int apiSource;
-    public boolean isModpack;
+    // Add visibility modifier
+    private int apiSource;
+    private boolean isModpack;
+
+    // Add constructor
+    public ModSource(int apiSource, boolean isModpack) {
+        this.apiSource = apiSource;
+        this.isModpack = isModpack;
+    }
+
+    // Add getter/setter methods
+    public int getApiSource() {
+        return apiSource;
+    }
+
+    public void setApiSource(int apiSource) {
+        this.apiSource = apiSource;
+    }
+
+    public boolean isModpack() {
+        return isModpack;
+    }
+
+    public void setModpack(boolean isModpack) {
+        this.isModpack = isModpack;
+    }
 }
