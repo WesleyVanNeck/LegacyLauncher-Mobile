bdc126a5c11f75903c6d9b1a653111a1120e37da
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:27 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/services/ProgressServiceKeeper.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/services/ProgressServiceKeeper.java
@@ -4,14 +4,25 @@ import android.content.Context;
 
 import net.kdt.pojavlaunch.progresskeeper.TaskCountListener;
 
-public class ProgressServiceKeeper implements TaskCountListener {
+public abstract class ProgressServiceKeeper implements TaskCountListener {
+    // Prevent direct instantiation of this class
+    private ProgressServiceKeeper() {}
+
+    // Encourage the use of subclasses by making this class abstract
+    public abstract int getTaskCount();
+
     private final Context context;
+
+    // Constructor to initialize the context
     public ProgressServiceKeeper(Context ctx) {
         this.context = ctx;
     }
 
+    // Override the onUpdateTaskCount() method to start the ProgressService if the task count is greater than zero
     @Override
     public void onUpdateTaskCount(int taskCount) {
-        if(taskCount > 0) ProgressService.startService(context);
+        if (context != null && taskCount > 0) {
+            ProgressService.startService(context);
+        }
     }
 }
