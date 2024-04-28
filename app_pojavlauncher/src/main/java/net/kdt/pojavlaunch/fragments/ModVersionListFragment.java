6762028984c47384cfd7a3ac54cb4774cb1a6644
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:28 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ModVersionListFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ModVersionListFragment.java
@@ -2,20 +2,25 @@ package net.kdt.pojavlaunch.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.OnLifecycleEvent;
 
 import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.Tools;
+import net.kdt.pojavlaunch.databinding.FragmentModVersionListBinding;
 import net.kdt.pojavlaunch.mirrors.DownloadMirror;
 import net.kdt.pojavlaunch.modloaders.ModloaderDownloadListener;
 import net.kdt.pojavlaunch.modloaders.ModloaderListenerProxy;
@@ -26,10 +31,9 @@ import java.io.IOException;
 
 public abstract class ModVersionListFragment<T> extends Fragment implements Runnable, View.OnClickListener, ExpandableListView.OnChildClickListener, ModloaderDownloadListener {
     public static final String TAG = "ForgeInstallFragment";
-    private ExpandableListView mExpandableListView;
+    private FragmentModVersionListBinding binding;
     private ProgressBar mProgressBar;
-    private LayoutInflater mInflater;
-    private View mRetryView;
+    private ModloaderListenerProxy taskProxy;
 
     public ModVersionListFragment() {
         super(R.layout.fragment_mod_version_list);
@@ -38,66 +42,73 @@ public abstract class ModVersionListFragment<T> extends Fragment implements Runn
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        this.mInflater = LayoutInflater.from(context);
+        requireActivity().getLifecycle().addObserver(this);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ((TextView)view.findViewById(R.id.title_textview)).setText(getTitleText());
-        mProgressBar = view.findViewById(R.id.mod_dl_list_progress);
-        mExpandableListView = view.findViewById(R.id.mod_dl_expandable_version_list);
-        mExpandableListView.setOnChildClickListener(this);
-        mRetryView = view.findViewById(R.id.mod_dl_retry_layout);
-        view.findViewById(R.id.forge_installer_retry_button).setOnClickListener(this);
-        ModloaderListenerProxy taskProxy = getTaskProxy();
-        if(taskProxy != null) {
-            mExpandableListView.setEnabled(false);
+        binding = FragmentModVersionListBinding.bind(view);
+        binding.titleTextView.setText(getTitleText());
+        mProgressBar = binding.modDlListProgress;
+        ExpandableListView expandableListView = binding.modDlExpandableVersionList;
+        expandableListView.setOnChildClickListener(this);
+        binding.forgeInstallerRetryButton.setOnClickListener(this);
+        taskProxy = getTaskProxy();
+        if (taskProxy != null) {
+            expandableListView.setEnabled(false);
             taskProxy.attachListener(this);
         }
         new Thread(this).start();
     }
 
-    @Override
-    public void onStop() {
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    private void onStop() {
         ModloaderListenerProxy taskProxy = getTaskProxy();
-        if(taskProxy != null) taskProxy.detachListener();
-        super.onStop();
+        if (taskProxy != null) taskProxy.detachListener();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        requireActivity().getLifecycle().removeObserver(this);
     }
 
     @Override
     public void run() {
+        T versions = null;
         try {
-            T versions = loadVersionList();
-            Tools.runOnUiThread(()->{
-                if(versions != null) {
-                    mExpandableListView.setAdapter(createAdapter(versions, mInflater));
-                }else{
-                    mRetryView.setVisibility(View.VISIBLE);
-                }
+            versions = loadVersionList();
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading version list", e);
+            Tools.runOnUiThread(() -> {
                 mProgressBar.setVisibility(View.GONE);
+                binding.modDlRetryLayout.setVisibility(View.VISIBLE);
             });
-        }catch (IOException e) {
-            Tools.runOnUiThread(()-> {
-                if (getContext() != null) {
-                    Tools.showError(getContext(), e);
-                    mRetryView.setVisibility(View.VISIBLE);
-                    mProgressBar.setVisibility(View.GONE);
-                }
-            });
+            return;
         }
+        Tools.runOnUiThread(() -> {
+            if (versions != null) {
+                mProgressBar.setVisibility(View.GONE);
+                binding.modDlRetryLayout.setVisibility(View.GONE);
+                binding.modDlExpandableVersionList.setAdapter(createAdapter(versions, requireActivity().getLayoutInflater()));
+            } else {
+                mProgressBar.setVisibility(View.GONE);
+                binding.modDlRetryLayout.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
-        mRetryView.setVisibility(View.GONE);
         mProgressBar.setVisibility(View.VISIBLE);
+        binding.modDlRetryLayout.setVisibility(View.GONE);
         new Thread(this).start();
     }
 
     @Override
     public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
-        if(ProgressKeeper.hasOngoingTasks()) {
+        if (ProgressKeeper.hasOngoingTasks()) {
             Toast.makeText(expandableListView.getContext(), R.string.tasks_ongoing, Toast.LENGTH_LONG).show();
             return true;
         }
@@ -106,54 +117,59 @@ public abstract class ModVersionListFragment<T> extends Fragment implements Runn
         Runnable downloadTask = createDownloadTask(forgeVersion, taskProxy);
         setTaskProxy(taskProxy);
         taskProxy.attachListener(this);
-        mExpandableListView.setEnabled(false);
+        expandableListView.setEnabled(false);
         new Thread(downloadTask).start();
         return true;
     }
 
     @Override
     public void onDownloadFinished(File downloadedFile) {
-        Tools.runOnUiThread(()->{
-            Context context = requireContext();
+        Tools.runOnUiThread(() -> {
             getTaskProxy().detachListener();
             setTaskProxy(null);
-            mExpandableListView.setEnabled(true);
-            // Read the comment in FabricInstallFragment.onDownloadFinished() to see how this works
-            getParentFragmentManager().popBackStackImmediate();
-            onDownloadFinished(context, downloadedFile);
+            binding.modDlExpandableVersionList.setEnabled(true);
+            requireActivity().getSupportFragmentManager().popBackStack();
+            onDownloadFinished(requireContext(), downloadedFile);
         });
     }
 
     @Override
     public void onDataNotAvailable() {
-        Tools.runOnUiThread(()->{
-            Context context = requireContext();
+        Tools.runOnUiThread(() -> {
             getTaskProxy().detachListener();
             setTaskProxy(null);
-            mExpandableListView.setEnabled(true);
-            Tools.dialog(context,
-                    context.getString(R.string.global_error),
-                    context.getString(getNoDataMsg()));
+            binding.modDlExpandableVersionList.setEnabled(true);
+            new AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.global_error)
+                    .setMessage(getNoDataMsg())
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show();
         });
     }
 
     @Override
     public void onDownloadError(Exception e) {
-        Tools.runOnUiThread(()->{
-            Context context = requireContext();
+        Tools.runOnUiThread(() -> {
             getTaskProxy().detachListener();
             setTaskProxy(null);
-            mExpandableListView.setEnabled(true);
-            Tools.showError(context, e);
+            binding.modDlExpandableVersionList.setEnabled(true);
+            Tools.showError(requireContext(), e);
         });
     }
 
     public abstract int getTitleText();
+
     public abstract int getNoDataMsg();
+
     public abstract ModloaderListenerProxy getTaskProxy();
+
     public abstract T loadVersionList() throws IOException;
+
     public abstract void setTaskProxy(ModloaderListenerProxy proxy);
+
     public abstract ExpandableListAdapter createAdapter(T versionList, LayoutInflater layoutInflater);
+
     public abstract Runnable createDownloadTask(Object selectedVersion, ModloaderListenerProxy listenerProxy);
+
     public abstract void onDownloadFinished(Context context, File downloadedFile);
 }
