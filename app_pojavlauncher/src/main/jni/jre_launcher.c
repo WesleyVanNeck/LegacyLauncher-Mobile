5f6e655c4c26cb1a39be1bed16215a5681bd0eb8
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:42 +0700

    improve

--- app_pojavlauncher/src/main/jni/jre_launcher.c
+++ app_pojavlauncher/src/main/jni/jre_launcher.c
@@ -22,25 +22,35 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 #include <android/log.h>
-#include <dlfcn.h>
 #include <errno.h>
 #include <jni.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-// Boardwalk: missing include
 #include <string.h>
+#include <sys/signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dlfcn.h>
 
 #include "log.h"
 #include "utils.h"
 #include "environ/environ.h"
 
-// Uncomment to try redirect signal handling to JVM
-// #define TRY_SIG2JVM
+#ifndef TRY_SIG2JVM
+#define JVM_handle_linux_signal NULL
+#endif
 
-// PojavLancher: fixme: are these wrong?
+#ifndef LOGD
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "VMLauncher", __VA_ARGS__)
+#endif
+
+#ifndef LOGE
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "VMLauncher", __VA_ARGS__)
+#endif
+
 #define FULL_VERSION "1.8.0-internal"
 #define DOT_VERSION "1.8"
 
@@ -51,11 +61,12 @@ static const char** const_appclasspath = NULL;
 static const jboolean const_javaw = JNI_FALSE;
 static const jboolean const_cpwildcard = JNI_TRUE;
 static const jint const_ergo_class = 0; // DEFAULT_POLICY
+
 static struct sigaction old_sa[NSIG];
 
 void (*__old_sa)(int signal, siginfo_t *info, void *reserved);
-int (*JVM_handle_linux_signal)(int signo, siginfo_t* siginfo, void* ucontext, int abort_if_unrecognized);
 
+#ifndef __ANDROID__
 void android_sigaction(int signal, siginfo_t *info, void *reserved) {
   printf("process killed with signal %d code %p addr %p\n", signal,info->si_code,info->si_addr);
   if (JVM_handle_linux_signal == NULL) { // should not happen, but still
@@ -69,6 +80,8 @@ void android_sigaction(int signal, siginfo_t *info, void *reserved) {
       errno = orig_errno;
   }
 }
+#endif
+
 typedef jint JNI_CreateJavaVM_func(JavaVM **pvm, void **penv, void *args);
 
 typedef jint JLI_Launch_func(int argc, char ** argv, /* main argc, argc */
@@ -84,44 +97,31 @@ typedef jint JLI_Launch_func(int argc, char ** argv, /* main argc, argc */
         jint ergo                               /* ergonomics class policy */
 );
 
-static jint launchJVM(int margc, char** margv) {
+static jint launchJVM(int margc, char** restrict argv) {
    void* libjli = dlopen("libjli.so", RTLD_LAZY | RTLD_GLOBAL);
 
-   // Boardwalk: silence
-   // LOGD("JLI lib = %x", (int)libjli);
    if (NULL == libjli) {
        LOGE("JLI lib = NULL: %s", dlerror());
        return -1;
    }
-   LOGD("Found JLI lib");
 
    JLI_Launch_func *pJLI_Launch =
           (JLI_Launch_func *)dlsym(libjli, "JLI_Launch");
-    // Boardwalk: silence
-    // LOGD("JLI_Launch = 0x%x", *(int*)&pJLI_Launch);
 
    if (NULL == pJLI_Launch) {
        LOGE("JLI_Launch = NULL");
        return -1;
    }
 
-   LOGD("Calling JLI_Launch");
-
-   return pJLI_Launch(margc, margv,
+   return pJLI_Launch(margc, argv,
                    0, NULL, // sizeof(const_jargs) / sizeof(char *), const_jargs,
                    0, NULL, // sizeof(const_appclasspath) / sizeof(char *), const_appclasspath,
                    FULL_VERSION,
                    DOT_VERSION,
-                   *margv, // (const_progname != NULL) ? const_progname : *margv,
-                   *margv, // (const_launcher != NULL) ? const_launcher : *margv,
+                   argv[0], // (const_progname != NULL) ? const_progname : argv[0],
+                   argv[0], // (const_launcher != NULL) ? const_launcher : argv[0],
                    (const_jargs != NULL) ? JNI_TRUE : JNI_FALSE,
                    const_cpwildcard, const_javaw, const_ergo_class);
-/*
-   return pJLI_Launch(argc, argv, 
-       0, NULL, 0, NULL, FULL_VERSION,
-       DOT_VERSION, *margv, *margv, // "java", "openjdk",
-       JNI_FALSE, JNI_TRUE, JNI_FALSE, 0);
-*/
 }
 
 /*
@@ -140,24 +140,16 @@ JNIEXPORT jint JNICALL Java_com_oracle_dalvik_VMLauncher_launchJVM(JNIEnv *env, 
 #endif
 
    jint res = 0;
-   // int i;
+   int i;
    //Prepare the signal trapper
    struct sigaction catcher;
    memset(&catcher,0,sizeof(sigaction));
    catcher.sa_sigaction = android_sigaction;
    catcher.sa_flags = SA_SIGINFO|SA_RESTART;
    // SA_RESETHAND;
-#define CATCHSIG(X) sigaction(X, &catcher, &old_sa[X])
-    CATCHSIG(SIGILL);
-    //CATCHSIG(SIGABRT);
-    CATCHSIG(SIGBUS);
-    CATCHSIG(SIGFPE);
-#ifdef TRY_SIG2JVM
-    CATCHSIG(SIGSEGV);
-#endif
-    CATCHSIG(SIGSTKFLT);
-    CATCHSIG(SIGPIPE);
-    CATCHSIG(SIGXFSZ);
+   for (i = 0; i < NSIG; i++) {
+       sigaction(i, &catcher, &old_sa[i]);
+   }
    //Signal trapper ready
 
     // Save dalvik JNIEnv pointer for JVM launch thread
@@ -170,14 +162,33 @@ JNIEXPORT jint JNICALL Java_com_oracle_dalvik_VMLauncher_launchJVM(JNIEnv *env, 
     }
 
     int argc = (*env)->GetArrayLength(env, argsArray);
-    char **argv = convert_to_char_array(env, argsArray);
+    char **argv = malloc(argc * sizeof(char*));
+    if (argv == NULL) {
+        LOGE("Failed to allocate memory for argv");
+        return -1;
+    }
+    for (i = 0; i < argc; i++) {
+        jstring element = (*env)->GetObjectArrayElement(env, argsArray, i);
+        const char* element_str = (*env)->GetStringUTFChars(env, element, NULL);
+        argv[i] = malloc(strlen(element_str) + 1);
+        if (argv[i] == NULL) {
+            LOGE("Failed to allocate memory for argv[%d]", i);
+            // handle error
+            return -1;
+        }
+        strcpy(argv[i], element_str);
+        (*env)->ReleaseStringUTFChars(env, element, element_str);
+    }
 
     LOGD("Done processing args");
 
     res = launchJVM(argc, argv);
 
     LOGD("Going to free args");
-    free_char_array(env, argsArray, argv);
+    for (i = 0; i < argc; i++) {
+        free(argv[i]);
+    }
+    free(argv);
 
     LOGD("Free done");
 
