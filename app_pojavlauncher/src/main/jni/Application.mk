dfaac124a3f57950c8b65e990a4973e363953050
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:23 +0700

    improve

--- app_pojavlauncher/src/main/jni/Application.mk
+++ app_pojavlauncher/src/main/jni/Application.mk
@@ -1,4 +1,7 @@
-# NDK_TOOLCHAIN_VERSION := 4.9
-APP_PLATFORM := android-21
-APP_STL := system
-# APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+# Use a more recent and supported toolchain version
+NDK_TOOLCHAIN_VERSION := clang
+APP_PLATFORM := android-29 # Use the latest Android platform
+APP_STL := c++_shared # Use the shared C++ library for better performance
+
+# Specify the desired ABIs for your app
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
