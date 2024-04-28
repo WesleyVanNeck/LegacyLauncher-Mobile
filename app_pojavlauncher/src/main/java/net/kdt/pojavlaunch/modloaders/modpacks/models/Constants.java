10bf0203ad6b0fc70b74a415615e360fa7aa790b
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:16 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/models/Constants.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/models/Constants.java
@@ -1,16 +1,15 @@
 package net.kdt.pojavlaunch.modloaders.modpacks.models;
 
-public class Constants {
-    private Constants(){}
+public final class Constants {
+    private Constants() {}
 
-    /** Types of modpack apis */
-    public static final int SOURCE_MODRINTH = 0x0;
-    public static final int SOURCE_CURSEFORGE = 0x1;
-    public static final int SOURCE_TECHNIC = 0x2;
+    // Types of modpack APIs
+    public static final int MODRINTH = 0x0;
+    public static final int CURSEFORGE = 0x1;
+    public static final int TECHNIC = 0x2;
 
-    /** Modrinth api, file environments */
+    // Modrinth API, file environments
     public static final String MODRINTH_FILE_ENV_REQUIRED = "required";
     public static final String MODRINTH_FILE_ENV_OPTIONAL = "optional";
     public static final String MODRINTH_FILE_ENV_UNSUPPORTED = "unsupported";
-
 }
