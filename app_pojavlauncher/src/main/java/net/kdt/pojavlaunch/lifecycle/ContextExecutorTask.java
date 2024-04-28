4f00294cc4e470cb2661cffe80da80e644eeec61
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-04-28 22:24:14 +0700

    improve

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/lifecycle/ContextExecutorTask.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/lifecycle/ContextExecutorTask.java
@@ -3,23 +3,43 @@ package net.kdt.pojavlaunch.lifecycle;
 import android.app.Activity;
 import android.content.Context;
 
+import javax.annotation.Nullable;
+
 /**
- * A ContextExecutorTask is a task that can dynamically change its behaviour, based on the context
- * used for its execution. This can be used to implement for ex. error/finish notifications from
- * background threads that may live with the Service after the activity that started them died.
+ * A {@code ContextExecutorTask} is a task that can dynamically change its behavior based on the context
+ * used for its execution. This can be used to implement, for example, error/finish notifications from
+ * background threads that may continue to live with the Service after the activity that started them
+ * has died.
  */
+@FunctionalInterface
 public interface ContextExecutorTask {
     /**
-     * ContextExecutor will execute this function first if a foreground Activity that was attached to the
-     * ContextExecutor is available.
+     * This method will be executed first if a foreground {@code Activity} that was attached to the
+     * {@code ContextExecutor} is available.
+     *
      * @param activity the activity
      */
-    void executeWithActivity(Activity activity);
+    void executeWithForegroundActivity(@Nullable Activity activity);
 
     /**
-     * ContextExecutor will execute this function if a foreground Activity is not available, but the app
+     * This method will be executed if a foreground {@code Activity} is not available, but the app
      * is still running.
+     *
      * @param context the application context
      */
-    void executeWithApplication(Context context);
+    void executeWithApplicationContext(@Nullable Context context);
+}
+
+/**
+ * A {@code ContextExecutor} is an object that can execute tasks based on the current context of the app.
+ * It can be used to implement, for example, error/finish notifications from background threads that may
+ * continue to live with the Service after the activity that started them has died.
+ */
+public interface ContextExecutor {
+    /**
+     * Executes the given {@code ContextExecutorTask} based on the current context of the app.
+     *
+     * @param task the task to execute
+     */
+    void execute(ContextExecutorTask task);
 }
