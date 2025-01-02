fc7e411adf86e43b212a697a2edb86b07bd125ff
WesleyVanNeck <wesleyvanneck1012@gmail.com>
2025-01-02 12:55:25 +0100

    Update JREUtils.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -208,10 +208,10 @@ public class JREUtils {
         envMap.put("MESA_GL_VERSION_OVERRIDE", "3.2");
         envMap.put("MESA_GLSL_VERSION_OVERRIDE", "320");
        // envMap.put("MESA_GLSL_CACHE_DIR", Tools.DIR_CACHE.getAbsolutePath());
-        //envMap.put("force_glsl_extensions_warn", "true");
+        envMap.put("force_glsl_extensions_warn", "true");
         envMap.put("allow_higher_compat_version", "true");
-     //   envMap.put("allow_glsl_extension_directive_midshader", "true");
-     //   envMap.put("MESA_LOADER_DRIVER_OVERRIDE", "zink");
+        envMap.put("allow_glsl_extension_directive_midshader", "true");
+        envMap.put("MESA_LOADER_DRIVER_OVERRIDE", "zink");
 	    
         envMap.put("LD_LIBRARY_PATH", LD_LIBRARY_PATH);
         envMap.put("PATH", jreHome + "/bin:" + Os.getenv("PATH"));
